5ae95a9f7af6313b0def72c55fa062c4b2c8a458
subaru-hello <octpsubaru@icloud.com>
2021-10-12 07:47:27 +0900

    [add] pagesフォルダにindexファイルを作成した。

---
+++ app/frontend/pages/top/index.vue
@@ -0,0 +1,14 @@
+<template>
+   <v-btn class=" elevation='2' ">
+      <router-link to= "/tasks" class="btn btn-dark mt-5">診断する</router-link>
+    </v-btn>
+</template>
+<script>
+ export default {
+   name: "TopIndex",
+ }
+</script>
+
+<style scoped>
+
+</style>
