41b0ad9aaf30c3195aa38b58022562012544ed3f
subaru-hello <octpsubaru@icloud.com>
2021-10-15 18:37:40 +0900

    [add] axiosを導入#23

---
+++ app/frontend/plugins/axios.js
@@ -0,0 +1,9 @@
+import axios from 'axios'
+
+let csrf_token = document.getElementsByName('csrf-token')[0].content
+const axiosInstance = axios.create({
+  baseURL: 'api',
+  headers: { 'X-CSRF-TOKEN': csrf_token }
+})
+
+export { axiosInstance }
