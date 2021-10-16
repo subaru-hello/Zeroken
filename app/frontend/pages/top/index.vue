0c2f1c0a28e815ae73b9ab293f3fe09c13fe42b4
subaru-hello <octpsubaru@icloud.com>
2021-10-16 11:15:29 +0900

    [add] フォームのルーティングを設定

--- app/frontend/pages/top/index.vue
+++ app/frontend/pages/top/index.vue
@@ -1,7 +1,12 @@
 <template>
+<v-app>
   <v-btn class="elevation='2'">
     <router-link to="/tasks" class="btn btn-dark mt-5">診断する</router-link>
   </v-btn>
+  <v-btn class="elevation='2'">
+    <router-link to="/register" class="btn btn-dark mt-5">ユーザー登録をする</router-link>
+  </v-btn>
+  </v-app>
 </template>
 <script>
 export default {
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root to: 'home#index'
   namespace :api, format: 'json' do
     resources :users, only: %i[ create ]
+    get 'validation/unique', to: 'validations#unique'
   end
   get "*path", to: "home#index"
 
