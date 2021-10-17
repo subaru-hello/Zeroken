e8334aaee73fb638673b3df5a63570507e210415
subaru-hello <octpsubaru@icloud.com>
2021-10-17 20:12:14 +0900

    [add] トップページを追加#27

---
+++ app/frontend/pages/PreliquoTop.vue
@@ -0,0 +1,18 @@
+<template>
+     <div>
+         <p>酒ジュールを提供します</p>
+         <p v-if="!!authUser">ログインしています</p>
+    </div>
+</template>  
+
+ <script>
+    import { mapGetters } from 'vuex';
+    export default {
+        data(){
+            return {};
+        },
+        computed: {
+            ...mapGetters('users', ['authUser']),
+        },
+    };
+  </script>
