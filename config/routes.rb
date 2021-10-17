11669120f9f5a1f0e176cdfa723219aeb39a9bbd
subaru-hello <octpsubaru@icloud.com>
2021-10-17 22:47:36 +0900

    [add] ルーティングを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   root to: 'home#index'
   namespace :api, format: 'json' do
-    resources :users , only: %i[ create ]
+    resources :users , only: %i[ create ] do
+      get 'tell_on', on: :collection
+    end
     resource :sessions, only: %i[ create destroy ]
     get 'validation/unique', to: 'validations#unique'
   end
