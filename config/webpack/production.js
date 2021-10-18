d93552d099a605bd513cb7acd6d38bfebed4b1ae
subaru-hello <octpsubaru@icloud.com>
2021-10-19 07:13:50 +0900

    [add]実行環境にresolveを追加

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -1,5 +1,22 @@
 process.env.NODE_ENV = process.env.NODE_ENV || 'production'
 
 const environment = require('./environment')
-
+const customConfig = {
+    resolve: {
+      fallback: {
+        dgram: false,
+        fs: false,
+        net: false,
+        tls: false,
+        child_process: false
+      }
+    }
+  };
+  environment.config.delete('node.dgram')
+  environment.config.delete('node.fs')
+  environment.config.delete('node.net')
+  environment.config.delete('node.tls')
+  environment.config.delete('node.child_process')
+  
+  environment.config.merge(customConfig);
 module.exports = environment.toWebpackConfig()
