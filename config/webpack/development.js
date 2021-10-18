f54eb1e824d48d0013b8770ee6597b9da6e8d1c6
subaru-hello <octpsubaru@icloud.com>
2021-10-19 07:05:13 +0900

    [add] webpackにloaderを追加。
    babel,style,css.file

--- config/webpack/development.js
+++ config/webpack/development.js
@@ -1,5 +1,23 @@
 process.env.NODE_ENV = process.env.NODE_ENV || 'development'
 
 const environment = require('./environment')
-
+const customConfig = {
+    resolve: {
+      fallback: {
+        dgram: false,
+        fs: false,
+        net: false,
+        tls: false,
+        child_process: false
+      }
+    }
+  };
+  
+  environment.config.delete('node.dgram')
+  environment.config.delete('node.fs')
+  environment.config.delete('node.net')
+  environment.config.delete('node.tls')
+  environment.config.delete('node.child_process')
+  
+  environment.config.merge(customConfig);
 module.exports = environment.toWebpackConfig()
