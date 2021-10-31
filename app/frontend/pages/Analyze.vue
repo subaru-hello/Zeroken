0c113c7b140b87f16e349f8db96a34f89f988842
subaru-hello <octpsubaru@icloud.com>
2021-10-31 21:39:33 +0900

    [add] お酒の強さを診断するロジックを追加した

--- app/frontend/pages/Analyze.vue
+++ app/frontend/pages/Analyze.vue
@@ -10,20 +10,24 @@
         md="12"
         lg="12"
       >
-        <v-card class="mx-auto my-5" outlined tile height="150">
-          <v-card-title class="headline" style="margin: 0% 40%; width: 50%">
+        <v-card class="mx-auto my-5 form" outlined tile height="150" id="form">
+          <v-card-title
+            class="headline"
+            style="margin: 0% 40%; width: 50%"
+            :id="'bigq' + question.num"
+          >
             Q{{ question.num }}.
             {{ question.title }}
           </v-card-title>
 
           <v-col cols="12" xs="12" sm="12" md="12" lg="12">
             <v-layout justify-center>
-              <v-card-title :id="'q-' + question.num">
+              <v-card-title>
                 <v-container>
                   <p class="text-center mx-auto py-3" style="font-size: 26px">
                     あなたの回答： {{ question.answer }}
                   </p>
-                  <v-radio-group :id="'q-' + question.num" mandatory row>
+                  <v-radio-group :id="'smallq' + question.num" mandatory row>
                     <v-radio
                       class="mx-1"
                       fab
@@ -64,12 +68,11 @@
           x-large
           :disabled="isVisible"
           :ripple="{ center: false, class: 'gray--text' }"
-          @click="createSchedule()"
+          @click="moveToNomivation()"
         >
           診断
         </v-btn>
       </v-col>
-      <!-- <div v-if="createSchedule()"></div> -->
     </v-row>
     <p
       v-if="isVisible"
@@ -78,7 +81,7 @@
       tile
       height="150"
     >
-      <span></span>が未回答です。
+      <span></span>未回答の項目があります。
     </p>
   </div>
 </template>
@@ -91,10 +94,64 @@ export default {
     return {
       isVisible: '',
       radios: null,
+      total_points: [],
+      sake_strongness: [],
     };
   },
   computed: {
     ...mapGetters('question', ['questions']),
+    totalPoints() {
+      return this.total_points;
+    },
+    createSchedule() {
+      var trueAnswers = this.questions;
+      const answer0 = trueAnswers[0]['answer'];
+      const answer1 = trueAnswers[1]['answer'];
+      const answer2 = trueAnswers[2]['answer'];
+      const answer3 = trueAnswers[3]['answer'];
+      const answer4 = trueAnswers[4]['answer'];
+      const answer5 = trueAnswers[5]['answer'];
+      const answer6 = trueAnswers[6]['answer'];
+      const answer7 = trueAnswers[7]['answer'];
+      const answer8 = trueAnswers[8]['answer'];
+      const answer9 = trueAnswers[9]['answer'];
+      const answer10 = trueAnswers[10]['answer'];
+      const answer11 = trueAnswers[11]['answer'];
+      const answer12 = trueAnswers[12]['answer'];
+
+      let answerFirst = answer0 === 1 ? -10.04 : answer0 === 2 ? 8.95 : 5.22; //重要
+      let answerSecond = answer1 === 1 ? -0.43 : answer1 === 2 ? -2.98 : 1.2;
+      let answerThird = answer2 === 1 ? 3.37 : answer2 === 2 ? 0.38 : 0.38;
+      let answerForth = answer3 === 1 ? -0.58 : answer3 === 2 ? -1.27 : 0.25;
+      let answerFifth = answer4 === 1 ? 0.31 : answer4 === 2 ? 0.36 : -1.03;
+      let answerSixth = answer5 === 1 ? 0 : answer5 === 2 ? -4.11 : 0.1;
+      let answerSeventh = answer6 === 1 ? -0.79 : answer6 === 2 ? 0.07 : 0.01;
+      let answerEighth = answer7 === 1 ? 0.83 : answer7 === 2 ? 0.62 : -0.24;
+      let answerNinth = answer8 === 1 ? -3.25 : answer8 === 2 ? 1.43 : -0.44;
+      let answerTenth = answer9 === 1 ? -3.25 : answer9 === 2 ? 1.43 : -0.44;
+      let answerEleventh = answer10 === 1 ? -10.07 : answer10 === 2 ? -0.79 : 10.8;
+      let answerTwelvth = answer11 === 1 ? 8.15 : answer11 === 2 ? -2.42 : 0.14;
+      let answerThirteenth = answer12 === 1 ? -4.34 : answer12 === 2 ? 2.69 : -0.19;
+
+      const sumResult =
+        answerFirst +
+        answerSecond +
+        answerThird +
+        answerForth +
+        answerFifth +
+        answerSixth +
+        answerSeventh +
+        answerEighth +
+        answerNinth +
+        answerTenth +
+        answerEleventh +
+        answerTwelvth +
+        answerThirteenth;
+      this.total_points = sumResult;
+      let AlcoholStrongness = sumResult > 0 ? '酒豪' : sumResult === 0 ? '凡人' : '下戸';
+      this.sake_strongness = AlcoholStrongness;
+      return AlcoholStrongness;
+    },
   },
   watch: {},
   mounted() {
@@ -124,285 +181,10 @@ export default {
       const targetRect = e.currentTarget.getBoundingClientRect();
       window.scrollTo(0, window.pageYOffset + targetRect.top);
     },
-    createSchedule() {
-      // 1いつも出る 2時々出る 3全くない
-      //1: q1が１だったら-10.04を返し、2だったらを5.22を返し、3だったら8.95を返す。を13項目作成する。。。
-
-      const targetArray = this.questions;
-      const Answers = Object.values(targetArray);
-      //出力['1','2','1','3']のようになる。
-
-      const result1 = (Answers) => {
-        //Answers[0]で1を取得することができる。
-        if (Answers[0].answer == 1) {
-          return -10.04;
-        } else if (targetArray[0].answer == 2) {
-          return 8.95;
-        } else {
-          return 5.22;
-        }
-      };
-      //2:  q1が１だったら-0.43を返し、2だったらを-2.98を返し、3だったら1.2を返す。
-
-      const result2 = (Answers) => {
-        //Answers[0]で1を取得することができる。
-        if (Answers[0].answer == 1) {
-          return -0.43;
-        } else if (targetArray[0].answer == 2) {
-          return -2.98;
-        } else {
-          return 1.2;
-        }
-      };
-      //3:  q1が１だったら3.37を返し、2だったらを-3.89を返し、3だったら0.38を返す。
-
-      const result3 = (Answers) => {
-        //Answers[0]で1を取得することができる。
-        if (Answers[0].answer == 1) {
-          return -3.37;
-        } else if (targetArray[0].answer == 2) {
-          return -3.89;
-        } else {
-          return 0.38;
-        }
-      };
-      //4:  q1が１だったら-0.58を返し、2だったらを-1.27を返し、3だったら0.25を返す。
-
-      const result4 = (Answers) => {
-        //Answers[0]で1を取得することができる。
-        if (Answers[0].answer == 1) {
-          return -0.58;
-        } else if (targetArray[0].answer == 2) {
-          return -1.27;
-        } else {
-          return 0.25;
-        }
-      };
-      //5:  q1が１だったら0.31を返し、2だったらを0.36を返し、3だったら-1.03を返す。
-
-      const result5 = (Answers) => {
-        //Answers[0]で1を取得することができる。
-        if (Answers[0].answer == 1) {
-          return 0.31;
-        } else if (targetArray[0].answer == 2) {
-          return 0.36;
-        } else {
-          return -1.03;
-        }
-      };
-      //6:  q1が１だったら0を返し、2だったらを-4.11を返し、3だったら0.1を返す。
-
-      const result6 = (Answers) => {
-        //Answers[0]で1を取得することができる。
-        if (Answers[0].answer == 1) {
-          return 0;
-        } else if (targetArray[0].answer == 2) {
-          return -4.11;
-        } else {
-          return 0.1;
-        }
-      };
-      //7:  q1が１だったら-0.79を返し、2だったらを0.07を返し、3だったら0.01を返す。
-
-      const result7 = (Answers) => {
-        //Answers[0]で1を取得することができる。
-        if (Answers[0].answer == 1) {
-          return -0.79;
-        } else if (targetArray[0].answer == 2) {
-          return 0.07;
-        } else {
-          return 0.01;
-        }
-      };
-      //8:  q1が１だったら0.83を返し、2だったら0.62を返し、3だったら-0.24を返す。
-
-      const result8 = (Answers) => {
-        //Answers[0]で1を取得することができる。
-        if (Answers[0].answer == 1) {
-          return 0.83;
-        } else if (targetArray[0].answer == 2) {
-          return 0.62;
-        } else {
-          return -0.24;
-        }
-      };
-      //9:  q1が１だったら-3.25を返し、2だったらを1.43を返し、3だったら-0.44を返す。
-
-      const result9 = (Answers) => {
-        //Answers[0]で1を取得することができる。
-        if (Answers[0].answer == 1) {
-          return -3.25;
-        } else if (targetArray[0].answer == 2) {
-          return 1.43;
-        } else {
-          return -0.44;
-        }
-      };
-      //10: q1が１だったら-1.88を返し、2だったらを0.04を返し、3だったら0.26を返す。
-
-      const result10 = (Answers) => {
-        //Answers[0]で1を取得することができる。
-        if (Answers[0].answer == 1) {
-          return -1.88;
-        } else if (targetArray[0].answer == 2) {
-          return 0.04;
-        } else {
-          return 0.26;
-        }
-      };
-      //11: q1が１だったら-10.07を返し、2だったらを0.19を返し、3だったら0.03を返す。
-
-      const result11 = (Answers) => {
-        //Answers[0]で1を取得することができる。
-        if (Answers[0].answer == 1) {
-          return -10.07;
-        } else if (targetArray[0].answer == 2) {
-          return 0.19;
-        } else {
-          return 0.03;
-        }
-      };
-      //12: q1が１だったら8.15を返し、2だったらを-2.42を返し、3だったら0.14を返す。
-      const result12 = (Answers) => {
-        //Answers[0]で1を取得することができる。
-        if (Answers[0].answer == 1) {
-          return 8.15;
-        } else if (targetArray[0].answer == 2) {
-          return -2.42;
-        } else {
-          return 0.14;
-        }
-      };
-      //13: q1が１だったら-4.34を返し、2だったらを2.69を返し、3だったら-0.19を返す。
-      const result13 = (Answers) => {
-        //Answers[0]で1を取得することができる。
-        if (Answers[0].answer == 1) {
-          return -4.34;
-        } else if (targetArray[0].answer == 2) {
-          return 2.69;
-        } else {
-          return -0.19;
-        }
-      };
-
-      //    if (Answers[0] === 1){
-      //         return params =0.1,
-      //         this.$router.push({ name: 'PreliquoTop',
-      //   params: {
-      //     id: 'X000001',
-      //     name: 'Mike and Tom'}
-      //          } );
-      //     }else{
-      //          this.$router.push({ name: 'PreliquoTop',
-      //   params: {
-      //     id: 'X000001',
-      //     name: 'Mike and Tom'}
-      //          } );
-      //     }
-      // }
-      const total_points =
-        result1 +
-        result2 +
-        result3 +
-        result4 +
-        result5 +
-        result6 +
-        result7 +
-        result8 +
-        result9 +
-        result10 +
-        result11 +
-        result12 +
-        result13;
-
-      const DrunkState = (total_points) => {
-        if (total_points === 0) {
-          return 0;
-        } else if (total_points < 0) {
-          return 1;
-        } else {
-          return 2;
-        }
-      };
-      //酒ケジュールを作成する。
-      //酔いたい度(血中アルコール濃度)
-      //爽快期(0.02,0.03,0.04)
-      //ほろ酔い期(0.05,0.07,0.10)
-      //酩酊期(0.16,0.23,0.30)
-
-      //下戸（キャラクターをつけても面白いかも)
-      //爽快期0.02
-      //ほろ酔い期0.05
-      //酩酊期0.16
-
-      //凡人
-      //爽快期0.03
-      //ほろ酔い期0.07
-      //酩酊期0.23
-
-      //酒豪（キャラクターをつけても面白いかも）
-      //爽快期0.04
-      //ほろ酔い期0.10
-      //酩酊期0.30
-
-      // S 血中アルコール濃度 mg/ml＝
-      //人それぞれある。度数が高いものを飲まないと酔わないという人もいる。（逆に、低いものなら何杯でもいけるという人もいる。)
-      //Ａ（アルコール量ｇ20）／B 体重ｋｇ×2/3✖/1000（0.70アルコール体内分布係数）
-      // = 15.6/体重(x)　＝血中濃度（y)
-      //例えば70kgの場合、血中濃度は0.22になり、下戸だと酩酊になる。
-      //S　血中アルコール濃度を求める
-      //A アルコール量を求める
-      //a: 酒の量(mL) × b: 度数または% / 100 × 比重 = 純アルコール量(g)
-      //500(mL) × 0.05 × 0.8 = 20(g)
-      //Alcoholモデル(polimorphism)(ハイボール、焼酎ロック、ビール、日本酒、ウィスキー、ワイン、缶チューハイ)
-
-      //title:string ビール
-      //percentages:integer　0.05
-      //amounts:integer 350
-      //pure_alcohol_intake 14
-      //description 最初はやっぱりビールでしょ
-
-      //title:string 缶チューハイ(スト缶一本)
-      //percentages:integer　0.08
-      //amounts:integer 500
-      //pure_alcohol_intake 32
-      //description 8%じゃないと酒じゃないっしょ
-
-      //title:string ワイン(グラス一杯)
-      //percentages:integer　0.15
-      //amounts:integer 120
-      //pure_alcohol_intake 12
-      //description 芳醇な葡萄な香り
-
-      //title:string 日本酒(1合)
-      //percentages:integer　0.15
-      //amounts:integer 180
-      //pure_alcohol_intake 22
-      //description 日本人が作った酒
-
-      //title:string 焼酎（コップ一杯)
-      //percentages:integer　0.25
-      //amounts:integer 100
-      //pure_alcohol_intake 20
-      //description 強い漢にこれいっぱい
-
-      //title:string ウィスキー(シングル)
-      //percentages:integer　0.40
-      //amounts:integer 30
-      //pure_alcohol_intake 10
-      //description 最初はやっぱりビールでしょ
-
-      //お酒の1単位（純アルコールにして 20ｇ）
-      //ビール5％500ml 日本酒15%1合180ml 焼酎25%0.6合110ml ウィスキー43%ダブル１杯60ml 缶チューハイ5%500ml
-
-      //B 体重
-      const alcohol_ratio = 0.8;
-
-      const AlcoholInBody = alcohol.amounts * alocohol.percentages * alcohol_ratio;
-      // const Shuchedule = =>{
-
-      // }
+    moveToNomivation() {
+      this.$router.push({ name: 'SelectNomivation' });
     },
+    diagnosisPush() {},
   },
 };
 </script>
