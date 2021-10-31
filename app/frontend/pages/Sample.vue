85b89f2d3deb0e583d484a69a00cd6727899a569
subaru-hello <octpsubaru@icloud.com>
2021-10-31 21:40:50 +0900

    [add] axiosを勉強するためのviewを追加した

---
+++ app/frontend/pages/Sample.vue
@@ -0,0 +1,71 @@
+<template>
+  <table>
+    <tbody>
+      <tr>
+        <th>TotalPoints</th>
+        <th>次の飲み会への気分</th>
+        <th>お酒の強さ</th>
+      </tr>
+      <tr v-for="a in analyze" :key="a.id">
+        <td>{{ a.total_points }}</td>
+        <td>{{ a.drunk_types }}</td>
+        <td>{{ a.resistance_types }}</td>
+        <td>{{ Sakenotuyosa }}</td>
+        <td>{{ sumTotalPoints }}</td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<script>
+import axios from '../plugins/axios';
+import { mapActions, mapGetters, mapState } from 'vuex';
+export default {
+  data: function () {
+    return {
+      analyze: {
+        total_points: '',
+        drunk_types: '',
+        resistance_types: '',
+      },
+      errors: '',
+    };
+  },
+  mounted() {
+    axios.get('analyzes').then((response) => (this.analyze = response.data));
+  },
+  computed: {
+    Sakenotuyosa() {
+      if (this.analyze.total_points > 0) {
+        return '下戸';
+      } else if (this.analyze.total_points === 0) {
+        return '普通';
+      } else {
+        return '酒豪';
+      }
+    },
+    sumTotalPoints() {
+      return this.analyze.total_points;
+    },
+  },
+  methods: {
+    // ...mapState('analyze',['analyzes']),
+    createAnalyze: function () {
+      axios
+        .post('/api/v1/analyzes', this.analyze)
+        .then((response) => {
+          response.data;
+          //   this.$router.push({ name: 'PreliquaTop', params: { id: e.id } });
+        })
+        .catch((error) => {
+          console.error(error);
+          if (error.response.data && error.response.data.errors) {
+            this.errors = error.response.data.errors;
+          }
+        });
+    },
+  },
+};
+</script>
+
+<style scoped></style>
---
+++ app/frontend/pages/SelectNomivation.vue
@@ -0,0 +1,52 @@
+<template>
+  <v-container>
+    <v-row justify="center" align-content="center">
+      <v-col cols="12" xs="12" sm="12" md="12" lg="12">
+        <h1 class="text-center" style="font-size: 50px">
+          次の飲み会ではどのくらい酔う予定ですか？
+        </h1>
+      </v-col>
+      <v-col justify="center" align-content="center">
+        <img :src="imgSrc" width="150" height="100" />
+        <p>酩酊</p>
+      </v-col>
+      <v-col justify="center" align-content="center">
+        <img :src="imgSrc" width="150" height="100" />
+        <p>ほろ酔い</p>
+      </v-col>
+      <v-col justify="center" align-content="center">
+        <img :src="imgSrc" width="150" height="100" />
+        <p>しっぽり</p>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+// import { alcohol  } from '../const/alcohol';
+// import { alcoholable  } from '../const/alcoholable';
+export default {
+  name: 'SelectNomivation',
+  data: function () {
+    return {
+      analyzes: [],
+    };
+  },
+  created() {
+    this.fetchAnalyzes();
+  },
+  methods: {
+    fetchAnalyzes() {
+      this.$axios
+        .get('analyzes')
+        .then((res) => (this.tasks = res.data))
+        .catch((err) => console.log(err.status));
+    },
+  },
+  computed: {
+    imgSrc() {
+      return require('../src/img/liquor.svg');
+    },
+  },
+};
+</script>
