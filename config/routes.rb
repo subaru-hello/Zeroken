83f9c0c6724454d76141316b0297605d021a04e5
subaru-hello <octpsubaru@icloud.com>
2021-10-31 21:50:24 +0900

    [add] analyze, alcoholのルーティングを追加した

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,15 @@
 Rails.application.routes.draw do
   root to: 'home#index'
   namespace :api, format: 'json' do
-    
+    namespace :v1 do
     resources :users , only: %i[ create index ] do
       get 'tell_on', on: :collection
     end
     resource :sessions, only: %i[ create destroy ]
+    resource :alcohols, only: %i[ create index ]
+    resource :analyzes
     get 'validation/unique', to: 'validations#unique'
   end
+end
   get "*path", to: "home#index"
 end
