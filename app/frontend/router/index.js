ee10aa2f4e039fed68abedd44c7f6583dd6d5d17
subaru-hello <octpsubaru@icloud.com>
2021-10-31 21:51:23 +0900

    [add] vue-routerにnomivationを追加した

--- app/frontend/router/index.js
+++ app/frontend/router/index.js
@@ -2,6 +2,8 @@ import Vue from 'vue';
 import Router from 'vue-router';
 
 import Analyze from '../pages/Analyze';
+import Sample from '../pages/Sample';
+import SelectNomivation from '../pages/SelectNomivation';
 import UserRegister from '../pages/UserRegister';
 import PreliquoTop from '../pages/PreliquoTop';
 import UserLogin from '../pages/UserLogin';
@@ -30,6 +32,16 @@ const router = new Router({
       path: '/analyze',
       component: Analyze,
       name: 'Analyze',
+    },
+    {
+      path: '/nomivation',
+      component: SelectNomivation,
+      name: 'SelectNomivation',
+    },
+    {
+      path: '/sample',
+      component: Sample,
+      name: 'Sample',
     },
   ],
 });
