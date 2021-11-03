b4c280f996621da1cddda87b60596c1919447d0a
subaru-hello <octpsubaru@icloud.com>
2021-11-03 12:04:05 +0900

    [add] UIを整えた

--- app/frontend/components/global/TheFooter.vue
+++ app/frontend/components/global/TheFooter.vue
@@ -1,8 +1,8 @@
 <template>
     <div>
-        <v-footer class="grey lighten-5">
+        <v-footer style="background-color: #165e83;">
             <v-row justify="center" no-gutters>
-                <v-btn v-for="link in links" :key="link" color="black" text rounded class="my-2">
+                <v-btn v-for="link in links" :key="link" color="white" text rounded class="my-2">
                     {{ link }}
                 </v-btn>
             </v-row>
