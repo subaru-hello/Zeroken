7d46a33001554da42bdcf5e0351dad99f5028013
subaru-hello <octpsubaru@icloud.com>
2021-11-03 12:07:02 +0900

    [add] 遷移後のリザルト画面のモック作成

--- app/frontend/pages/Sample.vue
+++ app/frontend/pages/Sample.vue
@@ -1,5 +1,49 @@
 <template>
-  <table>
+  <div>
+    <h1 class="text-center" style="font-size: 50px">
+      あなたは
+      <p class="text-center" style="font-size: 50px">
+        {{ Sakenotuyosa }} <img :src="beerSrc" width="150" height="100" />
+      </p>
+      酩酊に向けた酒ケジュール
+    </h1>
+    <v-layout>
+      <v-card class="text-center mx-auto my-5 form" elevation="2" shaped width="5000" id="form">
+        <v-card-title style="width: 100%" class="headline justify-center">
+          酒ケジュール
+        </v-card-title>
+        <v-container class="d-flex flex-row mb-6">
+          <v-card class="text-center mx-auto my-5 form" elevation="2" shaped width="500" id="form">
+            <v-card-title style="width: 100%" class="headline justify-center">
+              1st.ビール
+            </v-card-title>
+            <img :src="beerSrc" width="150" height="100" />
+          </v-card>
+          <v-card class="text-center mx-auto my-5 form" elevation="2" shaped width="500" id="form">
+            <v-card-title style="width: 100%" class="headline justify-center">
+              2nd. 日本酒
+            </v-card-title>
+            <img :src="sakeSrc" width="150" height="100" />
+          </v-card>
+          <v-card class="text-center mx-auto my-5 form" elevation="2" shaped width="500" id="form">
+            <v-card-title style="width: 100%" class="headline justify-center">
+              3rd. ハイボール
+            </v-card-title>
+
+            <img :src="imgSrc" width="150" height="100" />
+          </v-card>
+          <v-card class="text-center mx-auto my-5 form" elevation="2" shaped width="500" id="form">
+            <v-card-title style="width: 100%" class="headline justify-center">
+              4th ハイボール
+            </v-card-title>
+
+            <img :src="imgSrc" width="150" height="100" />
+          </v-card>
+        </v-container>
+      </v-card>
+    </v-layout>
+
+    <!-- <table>
     <tbody>
       <tr>
         <th>TotalPoints</th>
@@ -14,11 +58,12 @@
         <td>{{ sumTotalPoints }}</td>
       </tr>
     </tbody>
-  </table>
+  </table> -->
+  </div>
 </template>
 
 <script>
-import axios from '../plugins/axios';
+import { mapActions } from 'vuex';
 export default {
   data: function () {
     return {
@@ -30,9 +75,7 @@ export default {
       errors: '',
     };
   },
-  mounted() {
-    axios.get('analyzes').then((response) => (this.analyze = response.data));
-  },
+  mounted() {},
   computed: {
     Sakenotuyosa() {
       if (this.analyze.total_points > 0) {
@@ -46,21 +89,43 @@ export default {
     sumTotalPoints() {
       return this.analyze.total_points;
     },
+    imgSrc() {
+      return require('../src/img/liquor.svg');
+    },
+    sakeSrc() {
+      return require('../src/img/sake.svg');
+    },
+    drinkSrc() {
+      return require('../src/img/drink.svg');
+    },
+    beerSrc() {
+      return require('../src/img/beer.svg');
+    },
   },
   methods: {
-    createAnalyze: function () {
-      axios
-        .post('/api/v1/analyzes', this.analyze)
-        .then((response) => {
-          response.data;
-        })
-        .catch((error) => {
-          console.error(error);
-          if (error.response.data && error.response.data.errors) {
-            this.errors = error.response.data.errors;
-          }
-        });
+    ...mapActions(['fetchAnalyze']),
+    ...mapActions(['createAnalyze']),
+
+    onSubmit(e) {
+      e.preventDefault();
+      this.createAnalyze(this.analyze);
     },
+    // createAnalyze: function () {
+    //   axios
+    //     .post('/analyzes', this.analyze)
+    //     .then((response) => {
+    //       response.data;
+    //     })
+    //     .catch((error) => {
+    //       console.error(error);
+    //       if (error.response.data && error.response.data.errors) {
+    //         this.errors = error.response.data.errors;
+    //       }
+    //     });
+    // },
+  },
+  created() {
+    this.fetchAnalyze;
   },
 };
 </script>
