194f86d917dc33edbb92d4e3bc100095625c1b86
subaru-hello <octpsubaru@icloud.com>
2021-11-06 06:27:37 +0900

    [add] modelにスコープを追加

--- app/models/alcohol.rb
+++ app/models/alcohol.rb
@@ -1,12 +1,13 @@
 class Alcohol < ApplicationRecord
   has_many :alcohol_orders, dependent: :destroy
+  scope :random_weak, ->{ self.where('alcohol_percentage < ?', 7).shuffle[0..4] }
 
-  #scopeを定義
-  #strong
-  #order_byを使って、alcohol_percentageが５％から１０個取り出し、ランダムで提供する。
+  scope :random_strong, ->{ self.where('alcohol_percentage > ?', 7).shuffle[0..4]  }
+  # scopeを定義
+  # strong
+  # order_byを使って、alcohol_percentageが５％から１０個取り出し、ランダムで提供する。
 
-  #normal
+  # normal
 
-  #weakも設定する。
-
+  # weakも設定する。
 end
