2e9e4ca73b1467f7c4dcf463a91606675d2c2845
subaru-hello <octpsubaru@icloud.com>
2021-11-10 08:25:54 +0900

    [add] バリデーションを追加

--- app/controllers/api/v1/validations_controller.rb
+++ app/controllers/api/v1/validations_controller.rb
@@ -2,13 +2,9 @@ module Api
   module V1
     class ValidationsController < ApplicationController
       def unique
-        result =
-          if User.where(nickname: params[:nickname]).or(User.where(email: params[:email])).exists?
-            'exist'
-          else
-            'unique'
-          end
-        render json: result
+        users = User.where(id: params[:id]).where('nickname = ? or email= ?', params[:nickname], params[:email])
+        result = users.exists? ? 'exist' : 'unique'
+        render json: { mata: result }
       end
     end
   end
