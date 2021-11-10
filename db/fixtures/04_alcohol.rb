32194ee0b805e17ae82f44516b9b64e27c3511ff
subaru-hello <octpsubaru@icloud.com>
2021-11-10 18:46:36 +0900

    [add] seed_fuにアルコール情報を追加

--- db/fixtures/04_alcohol.rb
+++ db/fixtures/04_alcohol.rb
@@ -1,62 +1,67 @@
-Alcohol.seed do |s|
-    s.id = 1
-    s.name = 'beer'
-    s.alcohol_percentage = 5
-    s.alcohol_amount = 350
-    s.pure_alcohol_intake = 14
-    s.description ='とりあえずビール'
-    end
+Alcohol.seed(
+    {
+        name: 'beer',
+        alcohol_percentage: 5,
+        alcohol_amount: 350,
+        pure_alcohol_intake: 14,
+        description: 'とりあえずビールでお馴染みのビール。乾杯、そしてキンキンに冷えたビールを流し込もう。'
+    },
+   {
+        name: "ワイン",
+        alcohol_percentage: 12,
+        alcohol_amount: 120,
+        pure_alcohol_intake: 12,
+        description: "大人の味"
+    },
+    {
+        name: "ジントニック",
+        alcohol_percentage: 8,
+        alcohol_amount: 350,
+        pure_alcohol_intake: 22,
+        description: "コスパ良く酔える"
+    },
+    {
+        name: "ハイボール",
+        alcohol_percentage: 8,
+        alcohol_amount: 350,
+        pure_alcohol_intake: 22,
+        description: "酒はやっぱり8%っしょ！"
+    },
+    {
+        name: "日本酒",
+        alcohol_percentage: 12,
+        alcohol_amount: 180,
+        pure_alcohol_intake: 14,
+        description: "日本人は日本酒ですね"
+    },
+    {
+        name: "焼酎",
+        alcohol_percentage: 120,
+        alcohol_amount: 100,
+        pure_alcohol_intake: 18,
+        description: "男はやっぱりいも焼酎"
+    },
+    {
+        name: "レモンサワー",
+        alcohol_percentage: 5,
+        alcohol_amount: 350,
+        pure_alcohol_intake: 12,
+        description: "レモンサワーしか勝たん"
+    }
+)
+    # {
 
-    Alcohol.seed do |s|
-        s.id = 2,
-        s.name = "ワイン",
-        s.alcohol_percentage = 12,
-        s.alcohol_amount = 120,
-        s.pure_alcohol_intake = 12,
-        s.description = "大人の味"
-        end
+    # },
+    # {
 
-    Alcohol.seed do |s|
-        s.id = 3,
-        s.name = "ジントニック",
-        s.alcohol_percentage = 8,
-        s.alcohol_amount = 350,
-        s.pure_alcohol_intake = 22,
-        s.description = "コスパ良く酔える"
-        end
+    # },
+    # {
 
-    Alcohol.seed do |s|
-        s.id = 4,
-        s.name = "ハイボール",
-        s.alcohol_percentage = 8,
-        s.alcohol_amount = 350,
-        s.pure_alcohol_intake = 22,
-        s.description = "酒はやっぱり8%っしょ！"
-        end
+    # },
+    # {
 
-    Alcohol.seed do |s|
-        s.id = 5,
-        s.name = "日本酒",
-        s.alcohol_percentage = 12,
-        s.alcohol_amount = 180,
-        s.pure_alcohol_intake = 14,
-        s.description = "日本人は日本酒ですね"
-        end
+    # },
+    # {
 
-    # Alcohol.seed do |s|
-    #     s.id = 6,
-    #     s.name = "焼酎",
-    #     s.alcohol_percentage = 120,
-    #     s.alcohol_amount = 100,
-    #     s.pure_alcohol_intake = 18,
-    #     s.description = "男はやっぱりいも焼酎"
-    #     end
-
-    # Alcohol.seed do |s|
-    #     s.id = 7,
-    #     s.name = "レモンサワー",
-    #     s.alcohol_percentage = 5,
-    #     s.alcohol_amount =350,
-    #     s.pure_alcohol_intake = 12,
-    #     s.description = "レモンサワーしか勝たん"
-    #     end
+    # },
+ 
