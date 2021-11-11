f6c478fd9cf805801edaa6bbe6a03e3fdeec2175
subaru-hello <octpsubaru@icloud.com>
2021-11-12 07:45:10 +0900

    [add] seed_fuに画像を追加した

--- db/fixtures/04_alcohol.rb
+++ db/fixtures/04_alcohol.rb
@@ -1,52 +1,80 @@
 Alcohol.seed(
     {
-        name: 'beer',
-        alcohol_percentage: 5,
+        name: 'ビール',
+        alcohol_percentage: 5.09,
         alcohol_amount: 350,
         pure_alcohol_intake: 14,
-        description: 'とりあえずビールでお馴染みのビール。乾杯、そしてキンキンに冷えたビールを流し込もう。'
+        description: '発酵させた液体をろ過などを経てそのまま飲む「醸造酒」の一つ。とりあえずビールでお馴染みのビール。乾杯、そしてキンキンに冷えたビールを流し込もう。'
     },
    {
         name: "ワイン",
-        alcohol_percentage: 12,
+        alcohol_percentage: 12.02,
         alcohol_amount: 120,
         pure_alcohol_intake: 12,
-        description: "大人の味"
+        description: "発酵させた液体をろ過などを経てそのまま飲む「醸造酒」の一つ。ヨーロッパで嗜まれている葡萄仕立てのお酒。洋風の店に置いてあることが多い、大人の味。"
     },
     {
         name: "ジントニック",
-        alcohol_percentage: 8,
+        alcohol_percentage: 8.01,
         alcohol_amount: 350,
         pure_alcohol_intake: 22,
-        description: "コスパ良く酔える"
+        description: "ジンと水で割られたライム風味のお酒。爽やかなライムの香りが特徴。HUBで手軽に飲むことができる。コスパ良く酔いたい時におすすめ。"
     },
     {
         name: "ハイボール",
-        alcohol_percentage: 8,
+        alcohol_percentage: 8.01,
         alcohol_amount: 350,
         pure_alcohol_intake: 22,
-        description: "酒はやっぱり8%っしょ！"
+        description: "焼酎を炭酸水で割ったお酒。ダイエット中だけど酒は飲みたいという方が好んで飲む。コーラやジンジャーエールで割って楽しむことができる。食事の邪魔をしないシンプルな味わい。"
     },
     {
         name: "日本酒",
-        alcohol_percentage: 12,
+        alcohol_percentage: 12.01,
         alcohol_amount: 180,
         pure_alcohol_intake: 14,
-        description: "日本人は日本酒ですね"
+        description: "発酵させた液体をろ過などを経てそのまま飲む「醸造酒」の一つ。日本発祥のお酒で、海外ではsakeとして楽しまれている。"
     },
     {
-        name: "焼酎",
-        alcohol_percentage: 120,
+        name: "いも焼酎",
+        alcohol_percentage: 12.05,
         alcohol_amount: 100,
         pure_alcohol_intake: 18,
-        description: "男はやっぱりいも焼酎"
+        description: "「醸造酒」を熱し、水より沸点の低いアルコールを先に気化させ、冷やして液体に戻した「蒸留酒」の一つ。芋を原料にして作られた焼酎"
+    },
+    {
+        name: "麦焼酎",
+        alcohol_percentage: 12.06,
+        alcohol_amount: 100,
+        pure_alcohol_intake: 18,
+        description: "「醸造酒」を熱し、水より沸点の低いアルコールを先に気化させ、冷やして液体に戻した「蒸留酒」の一つ。麦を原料にして作られた焼酎"
     },
     {
         name: "レモンサワー",
-        alcohol_percentage: 5,
+        alcohol_percentage: 5.05,
         alcohol_amount: 350,
         pure_alcohol_intake: 12,
-        description: "レモンサワーしか勝たん"
+        description: "「醸造酒」や「蒸留酒」に薬草や果汁、香料などを加えた酒である「混成酒」の一つ。某有名男性ダンスグループに飲まれているお酒。お店によって度数の変動が激しい。"
+    },
+    {
+        name: "和み水",
+        alcohol_percentage: 0,
+        alcohol_amount: 0,
+        pure_alcohol_intake: 0,
+        description: "日本酒のお供に飲む水。日本酒のチェイサーとして知られている。"
+    },
+    {
+        name: "ウィスキー",
+        alcohol_percentage: 12.06,
+        alcohol_amount: 100,
+        pure_alcohol_intake: 14,
+        description: "「醸造酒」を熱し、水より沸点の低いアルコールを先に気化させ、冷やして液体に戻した「蒸留酒」の一つ。日本酒のお供に飲む水。日本酒のチェイサーとして知られている。"
+    },
+    {
+        name: "ウォッカ",
+        alcohol_percentage: 12.10,
+        alcohol_amount: 100,
+        pure_alcohol_intake: 14,
+        description: "「醸造酒」を熱し、水より沸点の低いアルコールを先に気化させ、冷やして液体に戻した「蒸留酒」の一つ。日本酒のお供に飲む水。日本酒のチェイサーとして知られている。"
     }
 )
     # {
