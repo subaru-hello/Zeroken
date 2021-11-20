b7e8744b5e47f3a295320aef912d011f9bc87408
subaru-hello <octpsubaru@icloud.com>
2021-11-21 08:49:52 +0900

    [add] お酒の種類を増やした。 内容は鳥貴族を参考

--- db/fixtures/04_alcohol.rb
+++ db/fixtures/04_alcohol.rb
@@ -2,22 +2,52 @@ Alcohol.seed(
     {
         name: 'ビール',
         alcohol_percentage: 5.01,
+        order: 0,
+        alcohol_amount: 350,
+        pure_alcohol_intake: 14,
+        description: '「醸造酒」の一つ。とりあえずビールでお馴染みのビール。乾杯、そしてキンキンに冷えたビールを流し込もう。'
+    },
+    {
+        name: 'ザ・プレミアムモルツ',
+        alcohol_percentage: 5.02,
+        order: 0,
         alcohol_amount: 350,
         pure_alcohol_intake: 14,
         description: '「醸造酒」の一つ。とりあえずビールでお馴染みのビール。乾杯、そしてキンキンに冷えたビールを流し込もう。'
     },
    {
-        name: "ワイン",
+        name: "赤ワイン",
         alcohol_percentage: 12.02,
         alcohol_amount: 120,
         pure_alcohol_intake: 12,
         description: "「醸造酒」の一つ。ヨーロッパで嗜まれている葡萄仕立てのお酒。洋風の店に置いてあることが多い、大人の味。"
     },
+   {
+        name: "白ワイン",
+        alcohol_percentage: 12.03,
+        alcohol_amount: 120,
+        pure_alcohol_intake: 12,
+        description: "「醸造酒」の一つ。ヨーロッパで嗜まれている葡萄仕立てのお酒。洋風の店に置いてあることが多い、大人の味。"
+    },
+    {
+        name: "ジンソーダ",
+        alcohol_percentage: 6.02,
+        alcohol_amount: 350,
+        pure_alcohol_intake: 16.8,
+        description: "ジンと水で割られたライム風味のお酒。爽やかなライムの香りが特徴。HUBで手軽に飲むことができる。コスパ良く酔いたい時におすすめ。"
+    },
     {
         name: "ジントニック",
-        alcohol_percentage: 8.02,
+        alcohol_percentage: 5.07,
         alcohol_amount: 350,
-        pure_alcohol_intake: 22,
+        pure_alcohol_intake: 1750.8,
+        description: "ジンと水で割られたライム風味のお酒。爽やかなライムの香りが特徴。HUBで手軽に飲むことができる。コスパ良く酔いたい時におすすめ。"
+    },
+    {
+        name: "ジントバック",
+        alcohol_percentage: 5.09,
+        alcohol_amount: 350,
+        pure_alcohol_intake: 1750.8,
         description: "ジンと水で割られたライム風味のお酒。爽やかなライムの香りが特徴。HUBで手軽に飲むことができる。コスパ良く酔いたい時におすすめ。"
     },
     {
@@ -28,38 +58,108 @@ Alcohol.seed(
         description: "ウィスキーをソーダ水で割ったお酒。ダイエット中だけど酒は飲みたいという方が好んで飲む。コーラやジンジャーエールで割って楽しむことができる。食事の邪魔をしないシンプルな味わい。"
     },
     {
-        name: "日本酒",
+        name: "ウーロンハイ",
+        alcohol_percentage: 5.03,
+        alcohol_amount: 350,
+        pure_alcohol_intake: 12,
+        description: "焼酎を烏龍茶で割ったお酒。あまり酔いたくないが、場の雰囲気的にお酒は口に運んでおいた方が無難だよなと判断した時に頼むことが多い。好き嫌いが分かれる。"
+    },
+    {
+        name: "緑茶ハイ",
+        alcohol_percentage: 5.04,
+        alcohol_amount: 350,
+        pure_alcohol_intake: 14,
+        description: "焼酎を緑茶で割ったお酒。炭酸が苦手な方におすすめ。これを頼む人は将来成功する人が多いと噂だ。"
+    },
+    {
+        name: "コークハイ",
+        alcohol_percentage: 5.11,
+        alcohol_amount: 350,
+        pure_alcohol_intake: 14,
+        description: "ウィスキーをコーラで割ったお酒。美味しく酔いたい時に頼むことが多い。悪酔いに注意が必要だ。"
+    },
+    {
+        name: "ジンジャーハイ",
+        alcohol_percentage: 5.12,
+        alcohol_amount: 350,
+        pure_alcohol_intake: 14,
+        description: "ウィスキーをジンジャーエールで割ったお酒。美味しく酔いたい時に頼むことが多い。ウィスキー割界においてコークハイと二大巨頭を張っている。"
+    },
+    {
+        name: "メガハイボール",
+        alcohol_percentage: 6.01,
+        alcohol_amount: 700,
+        pure_alcohol_intake: 33.6,
+        description: "ウィスキーをソーダ水で割ったお酒。ダイエット中だけど酒は飲みたいという方が好んで飲む。コーラやジンジャーエールで割って楽しむことができる。食事の邪魔をしないシンプルな味わい。"
+    },
+    {
+        name: "ウィスキー(ストレート)",
+        alcohol_percentage: 40.08,
+        alcohol_amount: 30,
+        pure_alcohol_intake: 1200,
+        description: "「蒸留酒」の一つ。ダンディーな雰囲気が漂う大人の飲み物。お好きでしょ。"
+    },
+    {
+        name: "ウィスキー(水割り)",
+        alcohol_percentage: 10.07,
+        alcohol_amount: 100,
+        pure_alcohol_intake: 1000,
+        description: "「蒸留酒」の一つ。ダンディーな雰囲気が漂う大人の飲み物。お好きでしょ。"
+    },
+    {
+        name: "ウィスキー(ロック)",
+        alcohol_percentage: 40.07,
+        alcohol_amount: 30,
+        pure_alcohol_intake: 1200,
+        description: "「蒸留酒」の一つ。ダンディーな雰囲気が漂う大人の飲み物。お好きでしょ。"
+    },
+    {
+        name: "日本酒(冷酒）",
         alcohol_percentage: 15.01,
         alcohol_amount: 180,
-        pure_alcohol_intake: 14,
+        pure_alcohol_intake: 2700,
         description: "「醸造酒」の一つ。日本発祥のお酒で、海外ではsakeとして楽しまれている。"
     },
     {
-        name: "いも焼酎",
-        alcohol_percentage: 20.05,
-        alcohol_amount: 100,
-        pure_alcohol_intake: 18,
+        name: "日本酒(熱燗)",
+        alcohol_percentage: 15.01,
+        alcohol_amount: 180,
+        pure_alcohol_intake: 2700,
+        description: "「醸造酒」の一つ。日本発祥のお酒で、海外ではsakeとして楽しまれている。"
+    },
+    {
+        name: "いも焼酎(ロック)",
+        alcohol_percentage: 25.05,
+        alcohol_amount: 90,
+        pure_alcohol_intake: 2250,
         description: "「蒸留酒」の一つ。芋を原料にして作られた焼酎。これを飲むとおじさんに可愛がられるようになる。"
     },
     {
         name: "麦焼酎",
-        alcohol_percentage: 20.06,
-        alcohol_amount: 100,
-        pure_alcohol_intake: 18,
+        alcohol_percentage: 25.06,
+        alcohol_amount: 90,
+        pure_alcohol_intake: 2250,
         description: "「蒸留酒」の一つ。麦を原料にして作られた焼酎。甘い匂いを出す。飲むと酩酊に近づくことができる。"
     },
     {
         name: "レモンサワー",
         alcohol_percentage: 5.05,
         alcohol_amount: 350,
-        pure_alcohol_intake: 12,
+        pure_alcohol_intake: 1750,
         description: "「混成酒」の一つ。某有名男性ダンスグループに飲まれているお酒。お店によって度数の変動が激しい。"
     },
     {
-        name: "少し濃いめのレモンサワー",
-        alcohol_percentage: 6.01,
+        name: "優しいレモンサワー",
+        alcohol_percentage: 3.05,
         alcohol_amount: 350,
-        pure_alcohol_intake: 12,
+        pure_alcohol_intake: 1050,
+        description: "「混成酒」の一つ。某有名男性ダンスグループに飲まれているお酒。お店によって度数の変動が激しい。"
+    },
+    {
+        name: "濃いめのレモンサワー",
+        alcohol_percentage: 7.01,
+        alcohol_amount: 350,
+        pure_alcohol_intake: 2450,
         description: "「混成酒」の一つ。レモンサワー専門店で提供されることが多い。度数は高めだが、呑みやすさに定評がある。"
     },
     {
@@ -77,49 +177,98 @@ Alcohol.seed(
         description: "炭酸系の酒を飲み終わった後の残骸。溶けると水になるため、お酒の濃度を薄めてくれる。お口が寂しい時は舐めよう。"
     },
     {
-        name: "ウィスキー",
-        alcohol_percentage: 40.07,
-        alcohol_amount: 100,
-        pure_alcohol_intake: 14,
-        description: "「蒸留酒」の一つ。ダンディーな雰囲気が漂う大人の飲み物。お好きでしょ。"
-    },
-    {
-        name: "ウーロンハイ",
-        alcohol_percentage: 5.03,
-        alcohol_amount: 350,
-        pure_alcohol_intake: 12,
-        description: "焼酎を烏龍茶で割ったお酒。あまり酔いたくないが、場の雰囲気的にお酒は口に運んでおいた方が無難だよなと判断した時に頼むことが多い。好き嫌いが分かれる。"
-    },
-    {
-        name: "緑茶ハイ",
-        alcohol_percentage: 5.04,
-        alcohol_amount: 350,
-        pure_alcohol_intake: 14,
-        description: "焼酎を緑茶で割ったお酒。炭酸が苦手な方におすすめ。これを頼む人は将来成功する人が多いと噂だ。"
-    },
-    {
         name: "梅酒",
-        alcohol_percentage: 13.10,
-        alcohol_amount: 100,
-        pure_alcohol_intake: 14,
+        alcohol_percentage: 11.50,
+        alcohol_amount: 60,
+        pure_alcohol_intake: 720,
         description: "6月頃に収穫される青梅を、蒸留酒（ホワイトリカー、焼酎、ブランデーが一般的）に漬け込むことで作られる混成酒類（アルコール飲料）の一種。甘くて美味しいが意外と度数が高い。飲み過ぎに注意だ。"
     },
     {
-        name: "コークハイ",
-        alcohol_percentage: 5.11,
-        alcohol_amount: 350,
-        pure_alcohol_intake: 14,
-        description: "ウィスキーをコーラで割ったお酒。美味しく酔いたい時に頼むことが多い。悪酔いに注意が必要だ。"
+        name: "みかん酒",
+        alcohol_percentage: 11.50,
+        alcohol_amount: 60,
+        pure_alcohol_intake: 720,
+        description: "6月頃に収穫される青梅を、蒸留酒（ホワイトリカー、焼酎、ブランデーが一般的）に漬け込むことで作られる混成酒類（アルコール飲料）の一種。甘くて美味しいが意外と度数が高い。飲み過ぎに注意だ。"
     },
     {
-        name: "ジンジャーハイ",
-        alcohol_percentage: 5.12,
+        name: "ファジーネーブル",
+        alcohol_percentage: 5.10,
         alcohol_amount: 350,
-        pure_alcohol_intake: 14,
-        description: "ウィスキーをジンジャーエールで割ったお酒。美味しく酔いたい時に頼むことが多い。ウィスキー割界においてコークハイと二大巨頭を張っている。"
+        pure_alcohol_intake: 1750,
+        description: "「醸造酒」を熱し、水より沸点の低いアルコールを先に気化させ、冷やして液体に戻した「蒸留酒」の一つ。ロシア生まれの強いお酒。酒豪にこれ一杯。"
     },
     {
-        name: "ウォッカ",
+        name: "柚ハチミツ",
+        alcohol_percentage: 3.10,
+        alcohol_amount: 350,
+        pure_alcohol_intake: 1050,
+        description: "「醸造酒」を熱し、水より沸点の低いアルコールを先に気化させ、冷やして液体に戻した「蒸留酒」の一つ。ロシア生まれの強いお酒。酒豪にこれ一杯。"
+    },
+    {
+        name: "モスコミュール",
+        alcohol_percentage: 5.10,
+        alcohol_amount: 350,
+        pure_alcohol_intake: 1750,
+        description: "「醸造酒」を熱し、水より沸点の低いアルコールを先に気化させ、冷やして液体に戻した「蒸留酒」の一つ。ロシア生まれの強いお酒。酒豪にこれ一杯。"
+    },
+    {
+        name: "メガ金麦",
+        alcohol_percentage: 12.10,
+        alcohol_amount: 100,
+        pure_alcohol_intake: 14,
+        description: "「醸造酒」を熱し、水より沸点の低いアルコールを先に気化させ、冷やして液体に戻した「蒸留酒」の一つ。ロシア生まれの強いお酒。酒豪にこれ一杯。"
+    },
+    {
+        name: "",
+        alcohol_percentage: 12.10,
+        alcohol_amount: 100,
+        pure_alcohol_intake: 14,
+        description: "「醸造酒」を熱し、水より沸点の低いアルコールを先に気化させ、冷やして液体に戻した「蒸留酒」の一つ。ロシア生まれの強いお酒。酒豪にこれ一杯。"
+    },
+    {
+        name: "",
+        alcohol_percentage: 12.10,
+        alcohol_amount: 100,
+        pure_alcohol_intake: 14,
+        description: "「醸造酒」を熱し、水より沸点の低いアルコールを先に気化させ、冷やして液体に戻した「蒸留酒」の一つ。ロシア生まれの強いお酒。酒豪にこれ一杯。"
+    },
+    {
+        name: "",
+        alcohol_percentage: 12.10,
+        alcohol_amount: 100,
+        pure_alcohol_intake: 14,
+        description: "「醸造酒」を熱し、水より沸点の低いアルコールを先に気化させ、冷やして液体に戻した「蒸留酒」の一つ。ロシア生まれの強いお酒。酒豪にこれ一杯。"
+    },
+    {
+        name: "",
+        alcohol_percentage: 12.10,
+        alcohol_amount: 100,
+        pure_alcohol_intake: 14,
+        description: "「醸造酒」を熱し、水より沸点の低いアルコールを先に気化させ、冷やして液体に戻した「蒸留酒」の一つ。ロシア生まれの強いお酒。酒豪にこれ一杯。"
+    },
+    {
+        name: "",
+        alcohol_percentage: 12.10,
+        alcohol_amount: 100,
+        pure_alcohol_intake: 14,
+        description: "「醸造酒」を熱し、水より沸点の低いアルコールを先に気化させ、冷やして液体に戻した「蒸留酒」の一つ。ロシア生まれの強いお酒。酒豪にこれ一杯。"
+    },
+    {
+        name: "",
+        alcohol_percentage: 12.10,
+        alcohol_amount: 100,
+        pure_alcohol_intake: 14,
+        description: "「醸造酒」を熱し、水より沸点の低いアルコールを先に気化させ、冷やして液体に戻した「蒸留酒」の一つ。ロシア生まれの強いお酒。酒豪にこれ一杯。"
+    },
+    {
+        name: "",
+        alcohol_percentage: 12.10,
+        alcohol_amount: 100,
+        pure_alcohol_intake: 14,
+        description: "「醸造酒」を熱し、水より沸点の低いアルコールを先に気化させ、冷やして液体に戻した「蒸留酒」の一つ。ロシア生まれの強いお酒。酒豪にこれ一杯。"
+    },
+    {
+        name: "",
         alcohol_percentage: 12.10,
         alcohol_amount: 100,
         pure_alcohol_intake: 14,
