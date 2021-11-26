c8f1a7dadb162265d9cfb5186a3a7d05e2fbe16b
subaru-hello <octpsubaru@icloud.com>
2021-11-26 17:30:13 +0900

    [add] seedを追加

--- db/fixtures/01_analyze.rb
+++ db/fixtures/01_analyze.rb
@@ -3,12 +3,12 @@ Analyze.seed do |s|
     s.total_points = 1
     s.sake_strongness_types ='strong'
     s.next_nomivation_types ='tipsy'
-    s.shuchedule = "1"
+    s.shuchedule = "3"
     end
 Analyze.seed do |s|
     s.id = 2
     s.total_points = 0
     s.sake_strongness_types ='normal'
     s.next_nomivation_types ='flesh'
-    s.shuchedule = '1'
+    s.shuchedule = '7'
     end
