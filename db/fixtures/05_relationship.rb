9b60ee5e7915772fbd3470da3f5a206424c9ad90
subaru-hello <octpsubaru@icloud.com>
2021-11-26 15:50:36 +0900

    [add] 酒ケジュールを入れるseedファイルを作成した

---
+++ db/fixtures/05_relationship.rb
@@ -0,0 +1,370 @@
+Relationship.seed(
+    {
+        liquor_box_id: 1,
+        liquor_id: 25
+    },
+    {
+        liquor_box_id: 1,
+        liquor_id: 64
+    },
+    {
+        liquor_box_id: 1,
+        liquor_id: 38
+    },
+    {
+        liquor_box_id: 1,
+        liquor_id: 28
+    },
+    {
+        liquor_box_id: 2,
+        liquor_id: 25
+    },
+    {
+        liquor_box_id: 2,
+        liquor_id: 64
+    },
+    {
+        liquor_box_id: 2,
+        liquor_id: 46
+    },
+    {
+        liquor_box_id: 2,
+        liquor_id: 42
+    },
+    {
+        liquor_box_id: 3,
+        liquor_id: 64
+    },
+    {
+        liquor_box_id: 3,
+        liquor_id: 46
+    },
+    {
+        liquor_box_id: 3,
+        liquor_id: 41
+    },
+    {
+        liquor_box_id: 3,
+        liquor_id: 42
+    },
+    {
+        liquor_box_id: 4,
+        liquor_id: 27
+    },
+    {
+        liquor_box_id: 4,
+        liquor_id: 39
+    },
+    {
+        liquor_box_id: 4,
+        liquor_id: 38
+    },
+    {
+        liquor_box_id: 4,
+        liquor_id: 45
+    },
+    {
+        liquor_box_id: 5,
+        liquor_id: 25
+    },
+    {
+        liquor_box_id: 5,
+        liquor_id: 45
+    },
+    {
+        liquor_box_id: 5,
+        liquor_id: 44
+    },
+    {
+        liquor_box_id: 5,
+        liquor_id: 43
+    },
+    {
+        liquor_box_id: 6,
+        liquor_id: 28
+    },
+    {
+        liquor_box_id: 6,
+        liquor_id: 38
+    },
+    {
+        liquor_box_id: 6,
+        liquor_id: 38
+    },
+    {
+        liquor_box_id: 6,
+        liquor_id: 59
+    },
+    {
+        liquor_box_id: 7,
+        liquor_id: 25
+    },
+    {
+        liquor_box_id: 7,
+        liquor_id: 42
+    },
+    {
+        liquor_box_id: 7,
+        liquor_id: 44
+    },
+    {
+        liquor_box_id: 7,
+        liquor_id: 45
+    },
+    {
+        liquor_box_id: 8,
+        liquor_id: 25
+    },
+    {
+        liquor_box_id: 8,
+        liquor_id: 38
+    },
+    {
+        liquor_box_id: 8,
+        liquor_id: 35
+    },
+    {
+        liquor_box_id: 8,
+        liquor_id: 34
+    },
+    {
+        liquor_box_id: 9,
+        liquor_id: 25
+    },
+    {
+        liquor_box_id: 9,
+        liquor_id: 28
+    },
+    {
+        liquor_box_id: 9,
+        liquor_id: 44
+    },
+    {
+        liquor_box_id: 9,
+        liquor_id: 43
+    },
+    {
+        liquor_box_id: 10,
+        liquor_id: 25
+    },
+    {
+        liquor_box_id: 10,
+        liquor_id: 43
+    },
+    {
+        liquor_box_id: 10,
+        liquor_id: 44
+    },
+    {
+        liquor_box_id: 10,
+        liquor_id: 41
+    },
+    {
+        liquor_box_id: 11,
+        liquor_id: 25
+    },
+    {
+        liquor_box_id: 11,
+        liquor_id: 33
+    },
+    {
+        liquor_box_id: 11,
+        liquor_id: 42
+    },
+    {
+        liquor_box_id: 11,
+        liquor_id: 50
+    },
+    {
+        liquor_box_id: 12,
+        liquor_id: 25
+    },
+    {
+        liquor_box_id: 12,
+        liquor_id: 28
+    },
+    {
+        liquor_box_id: 12,
+        liquor_id: 28
+    },
+    {
+        liquor_box_id: 12,
+        liquor_id: 28
+    },
+    {
+        liquor_box_id: 13,
+        liquor_id: 25
+    },
+    {
+        liquor_box_id: 13,
+        liquor_id: 31
+    },
+    {
+        liquor_box_id: 13,
+        liquor_id: 41
+    },
+    {
+        liquor_box_id: 13,
+        liquor_id: 33
+    },
+    {
+        liquor_box_id: 14,
+        liquor_id: 25
+    },
+    {
+        liquor_box_id: 14,
+        liquor_id: 33
+    },
+    {
+        liquor_box_id: 14,
+        liquor_id: 41
+    },
+    {
+        liquor_box_id: 14,
+        liquor_id: 59
+    },
+    {
+        liquor_box_id: 15,
+        liquor_id: 25
+    },
+    {
+        liquor_box_id: 15,
+        liquor_id: 32
+    },
+    {
+        liquor_box_id: 15,
+        liquor_id: 33
+    },
+    {
+        liquor_box_id: 15,
+        liquor_id: 34
+    },
+    {
+        liquor_box_id: 16,
+        liquor_id: 25
+    },
+    {
+        liquor_box_id: 16,
+        liquor_id: 33
+    },
+    {
+        liquor_box_id: 16,
+        liquor_id: 34
+    },
+    {
+        liquor_box_id: 16,
+        liquor_id: 56
+    },
+    {
+        liquor_box_id: 17,
+        liquor_id: 25
+    },
+    {
+        liquor_box_id: 17,
+        liquor_id: 31
+    },
+    {
+        liquor_box_id: 17,
+        liquor_id: 35
+    },
+    {
+        liquor_box_id: 17,
+        liquor_id: 52
+    },
+    {
+        liquor_box_id: 18,
+        liquor_id: 28
+    },
+    {
+        liquor_box_id: 18,
+        liquor_id: 26
+    },
+    {
+        liquor_box_id: 18,
+        liquor_id: 51
+    },
+    {
+        liquor_box_id: 18,
+        liquor_id: 52
+    },
+    {
+        liquor_box_id: 19,
+        liquor_id: 25
+    },
+    {
+        liquor_box_id: 19,
+        liquor_id: 47
+    },
+    {
+        liquor_box_id: 19,
+        liquor_id: 51
+    },
+    {
+        liquor_box_id: 19,
+        liquor_id: 53
+    },
+    {
+        liquor_box_id: 20,
+        liquor_id: 25
+    },
+    {
+        liquor_box_id: 20,
+        liquor_id: 34
+    },
+    {
+        liquor_box_id: 20,
+        liquor_id: 48
+    },
+    {
+        liquor_box_id: 20,
+        liquor_id: 54
+    },
+    {
+        liquor_box_id: 21,
+        liquor_id: 25
+    },
+    {
+        liquor_box_id: 21,
+        liquor_id: 35
+    },
+    {
+        liquor_box_id: 21,
+        liquor_id: 57
+    },
+    {
+        liquor_box_id: 21,
+        liquor_id: 56
+    },
+    {
+        liquor_box_id: 22,
+        liquor_id: 25
+    },
+    {
+        liquor_box_id: 22,
+        liquor_id: 58
+    },
+    {
+        liquor_box_id: 22,
+        liquor_id: 62
+    },
+    {
+        liquor_box_id: 22,
+        liquor_id: 56
+    },
+    {
+        liquor_box_id: 23,
+        liquor_id: 26
+    },
+    {
+        liquor_box_id: 23,
+        liquor_id: 57
+    },
+    {
+        liquor_box_id: 23,
+        liquor_id: 53
+    },
+    {
+        liquor_box_id: 23,
+        liquor_id: 54
+    },
+)
