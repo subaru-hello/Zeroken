30ea37c80c54c3eb1edba2c231b2dc86c4507d6b
subaru-hello <octpsubaru@icloud.com>
2021-11-30 13:02:33 +0900

    [add] コンポーネント切り分けのモックを作成した

---
+++ app/frontend/components/forms/InputModal.vue
@@ -0,0 +1,13 @@
+<template>
+  <select :value="value" @change="$emit('input', $event.target.value)">
+    <option v-for="(option, index) in options" :key="index" :selected="option === value">
+      {{ option }}
+    </option>
+  </select>
+</template>
+
+<script>
+export default {
+  props: ['options', 'value'],
+};
+</script>
