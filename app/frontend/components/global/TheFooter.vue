fe7d6c04a39477aa9acd83eaf5f03c7f96ff6f99
subaru-hello <octpsubaru@icloud.com>
2021-11-30 13:12:52 +0900

    [fix]Lint実行

--- app/frontend/components/global/TheFooter.vue
+++ app/frontend/components/global/TheFooter.vue
@@ -2,24 +2,29 @@
   <div>
     <v-footer style="background-color: rgb(222 184 135)">
       <v-row justify="center" no-gutters>
-        <v-btn v-for="link in links" 
-        :key="link"
-        color="white" 
-        text rounded class="my-2"  
-        justify="center"
-        align-content="center"
-        @click="dialog = true">
+        <v-btn
+          v-for="link in links"
+          :key="link"
+          color="white"
+          text
+          rounded
+          class="my-2"
+          justify="center"
+          align-content="center"
+          @click="dialog = true"
+        >
           {{ link }}
         </v-btn>
       </v-row>
-          <v-dialog v-model="dialog" scrollable max-width="80%">
-            <v-card>
-              <v-card-title>作成中です。</v-card-title>
-              <v-divider></v-divider>
-              <v-card-text>大変申し訳ありません。作者が二日酔いのため作成が遅れております。しばしお待ちください。。
-              </v-card-text>
-            </v-card>
-          </v-dialog>
+      <v-dialog v-model="dialog" scrollable max-width="80%">
+        <v-card>
+          <v-card-title>作成中です。</v-card-title>
+          <v-divider></v-divider>
+          <v-card-text
+            >大変申し訳ありません。作者が二日酔いのため作成が遅れております。しばしお待ちください。。
+          </v-card-text>
+        </v-card>
+      </v-dialog>
     </v-footer>
   </div>
 </template>
