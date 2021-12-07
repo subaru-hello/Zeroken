9769b8067c38e2c453f6348cd2a29ec175da147f
subaru-hello <octpsubaru@icloud.com>
2021-12-08 06:25:29 +0900

    [fix] index.jsの不要な記述を削除した

--- app/frontend/store/index.js
+++ app/frontend/store/index.js
@@ -1,7 +1,6 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
 import users from './modules/users';
-import alcohol from './modules/alcohol';
 import analyze from './modules/analyze';
 import snackbar from './modules/snackbar';
 import question from './modules/question';
@@ -9,8 +8,6 @@ import question from './modules/question';
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  // eslint-disable-next-line no-undef
-  // strict: process.env.NODE_ENV !== 'production',
   modules: {
     users,
     alcohol,
