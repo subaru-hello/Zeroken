68282babaeb5f0605067bd88d076372603f93f29
subaru-hello <octpsubaru@icloud.com>
2021-12-08 18:32:20 +0900

    [fix] migrationファイルを編集した

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_08_071928) do
+ActiveRecord::Schema.define(version: 2021_12_08_085550) do
 
   create_table "active_storage_attachments", charset: "utf8mb4", force: :cascade do |t|
     t.string "name", null: false
@@ -66,8 +66,8 @@ ActiveRecord::Schema.define(version: 2021_12_08_071928) do
     t.datetime "updated_at", precision: 6, null: false
     t.text "description"
     t.integer "shuchedule"
-    t.string "next_motivation"
-    t.string "alcohol_strongness"
+    t.integer "next_motivation", default: 0
+    t.integer "alcohol_strongness", default: 0
     t.index ["user_id"], name: "index_analyzes_on_user_id"
   end
 
