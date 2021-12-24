174d6e64c81f03aa04bcb86ebf1b67af739ac2ce
subaru-hello <octpsubaru@icloud.com>
2021-12-24 12:23:16 +0900

    [add] 利用規約 プライバシーポリシー作成

---
+++ app/frontend/pages/PrivacyPolicy.vue
@@ -0,0 +1,299 @@
+<template>
+  <v-container class="privacy-policy">
+    <v-row class="pt-5 mb-3">
+      <v-col cols="auto">
+        <div class="mb-5 text-h4 font-weight-bold">プライバシーポリシー</div>
+        <div class="text-body-1 font-weight-medium">
+          このプライベートポリシー(以下、「本ポリシー」といいます。)は，このウェブサイト上で提供するサービス（以下，「本サービス」といいます。）における，ユーザーの個人情報の取り扱いについて定めるものです。登録ユーザーの皆さま（以下，「ユーザー」といいます。）には本ポリシーに従って，本サービスをご利用いただきます。
+          <br class=".br-pc" />
+          ユーザーは本サービスに利用登録することにより，本ポリシーの全ての記載内容について同意したものとみなされます。
+        </div>
+      </v-col>
+    </v-row>
+
+    <v-row class="mb-3">
+      <v-col cols="auto">
+        <div class="mb-5 text-h5 font-weight-bold">第1条（個人情報の定義）</div>
+        <div class="pl-5 text-body-1 font-weight-medium d-flex">
+          <div>
+            「個人情報」とは，個人情報の保護に関する法律に規定される生存する個人に関する情報(氏名、生年月日、その他の特定の個人を識別することができる情報)、ならびに特定の個人と結びついて使用されるメールアドレス、ユーザーＩＤ、パスワードなどの情報と認識しています。
+          </div>
+        </div>
+      </v-col>
+    </v-row>
+
+    <v-row class="mb-3">
+      <v-col cols="auto">
+        <div class="mb-5 text-h5 font-weight-bold">第2条（クッキー・IPアドレス情報）</div>
+        <div class="pl-5 text-body-1 font-weight-medium">
+          <div class="mb-5">
+            クッキーおよびＩＰアドレス情報(以下、クッキー等)については、それら単独では、特定の個人を識別することができないため、個人情報とは考えておりません。本サービスではプロフィールページへのログインの際や、本サービス上での全体傾向やパターンなどの情報を収集などにクッキーを使用しておりますが、ユーザーのプライバシーを侵害するものではありません。
+          </div>
+          <div>
+            Googleを含む第三者配信事業者は、ファーストパーティ Cookie（Googleアナリティクスの
+            Cookieなど）を 本サービスを訪れたユーザーの属性・興味・関心の分析に利用しております。
+          </div>
+        </div>
+      </v-col>
+    </v-row>
+
+    <v-row class="mb-3">
+      <v-col cols="auto">
+        <div class="mb-5 text-h5 font-weight-bold">第3条（個人情報の利用目的）</div>
+        <div class="pl-5 text-body-1 font-weight-medium">
+          <div class="mb-5">本サービスが個人情報を収集・利用する目的は，以下のとおりです</div>
+        </div>
+        <div class="pl-5 text-body-1 font-weight-medium">
+          <div class="mb-2 d-flex">
+            <div class="mr-2">1.</div>
+            <div>本サービスの提供・運営のため</div>
+          </div>
+          <div class="mb-2 d-flex">
+            <div class="mr-2">2.</div>
+            <div>ユーザーからのお問い合わせに回答するため（本人確認を行うことを含む）</div>
+          </div>
+          <div class="mb-2 d-flex">
+            <div class="mr-2">3.</div>
+            <div>
+              利用規約に違反したユーザーや，不正・不当な目的でサービスを利用しようとするユーザーの特定をし，ご利用をお断りするため
+            </div>
+          </div>
+          <div class="mb-2 d-flex">
+            <div class="mr-2">4.</div>
+            <div>
+              ユーザーにご自身の登録情報の閲覧や変更，削除，ご利用状況の閲覧を行っていただくため
+            </div>
+          </div>
+        </div>
+      </v-col>
+    </v-row>
+
+    <v-row class="mb-3">
+      <v-col cols="auto">
+        <div class="mb-5 text-h5 font-weight-bold">第4条（利用目的の変更）</div>
+        <div class="pl-5 text-body-1 font-weight-medium">
+          <div class="mb-2 d-flex">
+            <div class="mr-2">1.</div>
+            <div>
+              当社は，利用目的が変更前と関連性を有すると合理的に認められる場合に限り，個人情報の利用目的を変更するものとします。
+            </div>
+          </div>
+          <div class="mb-2 d-flex">
+            <div class="mr-2">2.</div>
+            <div>
+              利用目的の変更を行った場合には，変更後の目的について当社所定の方法により，ユーザーに通知，または公表するものとします。
+            </div>
+          </div>
+        </div>
+      </v-col>
+    </v-row>
+
+    <v-row class="mb-3">
+      <v-col cols="auto">
+        <div class="mb-5 text-h5 font-weight-bold">第5条（個人情報の第三者提供）</div>
+        <div class="pl-5 text-body-1 font-weight-medium">
+          <div class="mb-5 d-flex">
+            <div class="mr-2">1.</div>
+            <div>
+              本サービスは，次に掲げる場合を除くほか，あらかじめご本人の同意を得ないで、個人情報を第三者に提供しません。なお、本サービスは、メールアドレスなど直接特定の個人を識別することができる情報を除外した統計情報を第三者に開示する場合があります。
+            </div>
+          </div>
+          <div class="pl-5">
+            <div class="mb-5 d-flex">
+              <div class="mr-2">1.</div>
+              <div>
+                人の生命，身体または財産の保護のために必要がある場合であって，本人の同意を得ることが困難であるとき
+              </div>
+            </div>
+            <div class="mb-5 d-flex">
+              <div class="mr-2">2.</div>
+              <div>
+                公衆衛生の向上または児童の健全な育成の推進のために特に必要がある場合であって，本人の同意を得ることが困難であるとき
+              </div>
+            </div>
+            <div class="mb-5 d-flex">
+              <div class="mr-2">3.</div>
+              <div>
+                国の機関もしくは地方公共団体またはその委託を受けた者が法令の定める事務を遂行することに対して協力する必要がある場合であって，本人の同意を得ることにより当該事務の遂行に支障を及ぼすおそれがあるとき
+              </div>
+            </div>
+            <div class="mb-5 d-flex">
+              <div class="mr-2">4.</div>
+              <div>
+                ユーザーが本サービスにおいて他のユーザーを含む第三者に表示する目的で当社に提供した情報（登録情報、投稿情報など）を本サービス上に表示する場合
+              </div>
+            </div>
+          </div>
+
+          <div class="mb-5 d-flex">
+            <div class="mr-2">2.</div>
+            <div>
+              前項の定めにかかわらず，次に掲げる場合には，当該情報の提供先は第三者に該当しないものとします。
+            </div>
+          </div>
+          <div class="pl-5">
+            <div class="mb-5 d-flex">
+              <div class="mr-2">1.</div>
+              <div>
+                当社が利用目的の達成に必要な範囲内において個人情報の取扱いの全部または一部を委託する場合
+              </div>
+            </div>
+            <div class="mb-5 d-flex">
+              <div class="mr-2">2.</div>
+              <div>合併その他の事由による事業の承継に伴って個人情報が提供される場合</div>
+            </div>
+          </div>
+        </div>
+      </v-col>
+    </v-row>
+
+    <v-row class="mb-3">
+      <v-col cols="auto">
+        <div class="mb-5 text-h5 font-weight-bold">第6条（個人情報の開示）</div>
+        <div class="pl-5 text-body-1 font-weight-medium">
+          <div class="mb-5 d-flex">
+            <div class="mr-2">1.</div>
+            <div>
+              当サービスは，本人から個人情報の開示を求められたときは，本人に対し，遅滞なくこれを開示します。ただし，開示することにより次のいずれかに該当する場合は，その全部または一部を開示しないこともあり，開示しない決定をした場合には，その旨を遅滞なく通知します。
+            </div>
+          </div>
+          <div class="pl-5">
+            <div class="mb-5 d-flex">
+              <div class="mr-2">1.</div>
+              <div>本人または第三者の生命，身体，財産その他の権利利益を害するおそれがある場合</div>
+            </div>
+            <div class="mb-5 d-flex">
+              <div class="mr-2">2.</div>
+              <div>当サービスの適正な実施に著しい支障を及ぼすおそれがある場合</div>
+            </div>
+            <div class="mb-5 d-flex">
+              <div class="mr-2">3.</div>
+              <div>その他法令に違反することとなる場合</div>
+            </div>
+          </div>
+
+          <div class="mb-5 d-flex">
+            <div class="mr-2">2.</div>
+            <div>
+              前項の定めにかかわらず，履歴情報および特性情報などの個人情報以外の情報については，原則として開示いたしません。
+            </div>
+          </div>
+        </div>
+      </v-col>
+    </v-row>
+
+    <v-row class="mb-3">
+      <v-col cols="auto">
+        <div class="mb-5 text-h5 font-weight-bold">第7条（個人情報の訂正および削除）</div>
+        <div class="pl-5 text-body-1 font-weight-medium">
+          <div class="mb-5 d-flex">
+            <div class="mr-2">1.</div>
+            <div>
+              ユーザーは，当サービスの保有する自己の個人情報が誤った情報である場合には，当サービスが定める手続きにより，当サービスに対して個人情報の訂正，追加または削除（以下，「訂正等」といいます。）を請求することができます。
+            </div>
+          </div>
+          <div class="mb-5 d-flex">
+            <div class="mr-2">2.</div>
+            <div>
+              当サービスは，ユーザーから前項の請求を受けてその請求に応じる必要があると判断した場合には，遅滞なく，当該個人情報の訂正等を行うものとします。
+            </div>
+          </div>
+          <div class="mb-5 d-flex">
+            <div class="mr-2">3.</div>
+            <div>
+              当サービスは，前項の規定に基づき訂正等を行った場合，または訂正等を行わない旨の決定をしたときは遅滞なく，これをユーザーに通知します。
+            </div>
+          </div>
+        </div>
+      </v-col>
+    </v-row>
+
+    <v-row class="mb-3">
+      <v-col cols="auto">
+        <div class="mb-5 text-h5 font-weight-bold">第8条（個人情報の利用停止等）</div>
+        <div class="pl-5 text-body-1 font-weight-medium">
+          <div class="mb-5 d-flex">
+            <div class="mr-2">1.</div>
+            <div>
+              当サービスは，本人から，個人情報が，利用目的の範囲を超えて取り扱われているという理由，または不正の手段により取得されたものであるという理由により，その利用の停止または消去（以下，「利用停止等」といいます。）を求められた場合には，遅滞なく必要な調査を行います。
+            </div>
+          </div>
+          <div class="mb-5 d-flex">
+            <div class="mr-2">2.</div>
+            <div>
+              前項の調査結果に基づき，その請求に応じる必要があると判断した場合には，遅滞なく，当該個人情報の利用停止等を行います。
+            </div>
+          </div>
+          <div class="mb-5 d-flex">
+            <div class="mr-2">3.</div>
+            <div>
+              当サービスは，前項の規定に基づき利用停止等を行った場合，または利用停止等を行わない旨の決定をしたときは，遅滞なく，これをユーザーに通知します。
+            </div>
+          </div>
+          <div class="mb-5 d-flex">
+            <div class="mr-2">4.</div>
+            <div>
+              前2項にかかわらず，利用停止等に多額の費用を有する場合その他利用停止等を行うことが困難な場合であって，ユーザーの権利利益を保護するために必要なこれに代わるべき措置をとれる場合は，この代替策を講じるものとします。
+            </div>
+          </div>
+        </div>
+      </v-col>
+    </v-row>
+
+    <v-row class="mb-3">
+      <v-col cols="auto">
+        <div class="mb-5 text-h5 font-weight-bold">第9条（プライバシーポリシーの変更）</div>
+        <div class="pl-5 text-body-1 font-weight-medium">
+          <div class="mb-5 d-flex">
+            <div class="mr-2">1.</div>
+            <div>
+              本ポリシーの内容は，法令その他本ポリシーに別段の定めのある事項を除いて，ユーザーに通知することなく，変更することができるものとします。
+            </div>
+          </div>
+          <div class="mb-5 d-flex">
+            <div class="mr-2">2.</div>
+            <div>
+              変更後のプライバシーポリシーは，本ウェブサイトに掲載したときから効力を生じるものとします。
+            </div>
+          </div>
+        </div>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  mounted() {
+    this.$toTop();
+    document.title = 'プライバシーポリシー - ZEROKEN';
+  },
+};
+</script>
+
+<style scoped>
+h1 {
+  margin-top: 20px;
+  margin-bottom: 10px;
+}
+h2,
+h3 {
+  margin-top: 15px;
+}
+.v-application p {
+  line-height: 1.7rem;
+  margin-top: 0.3rem;
+  margin-left: 10px;
+  margin-bottom: 0.3rem;
+}
+ul {
+  margin-left: 15px;
+}
+ul li {
+  line-height: 1.7rem;
+}
+.privacy-policy {
+  /* margin-bottom: 20px; */
+  background-color: rgba(255, 255, 255, 0.7);
+}
+</style>
