f190c6d7335aeafeb76710f6fc6fc9f2a9cf759c
runteq-profile <octpsubaru@icloud.com>
2022-01-16 15:53:17 +0900

    fix: 不要なファイルを削除

--- app/serializers/analyze_serializer.rb
+++
@@ -1,7 +0,0 @@
-class AnalyzeSerializer
-  include JSONAPI::Serializer
-  set_type :analyze
-  attributes :total_points, :alcohol_strongness, :next_motivation, :description, :shuchedule
-  belongs_to :user
-  has_many :my_shuchedules
-end
--- db/seeds/production/02_user.rb
+++
@@ -1,9 +0,0 @@
-User.seed do |s|
-        s.id = 1
-        s.email = 'test@example.com'
-        s.nickname = 'テス太郎'
-        s.password = 'aaaaaaa'
-        s.password_confirmation = 'aaaaaaa'
-        s.avatar = Rails.root.join("db/fixtures/images/sample.png").open
-        s.role = 0
- end
