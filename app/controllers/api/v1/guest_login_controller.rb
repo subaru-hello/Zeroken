6b5719b302cfe00f401f25b80b9e9917d815f930
runteq-profile <octpsubaru@icloud.com>
2022-01-18 08:49:12 +0900

    ファイル修正

--- app/controllers/api/v1/guest_login_controller.rb
+++ app/controllers/api/v1/guest_login_controller.rb
@@ -3,6 +3,7 @@ module Api
     class GuestLoginController < BaseController
       def create
         user = User.find_by!(role: :guest)
+        set_access_token!(user)
         auto_login(user)
         json_string = UserSerializer.new(user).serializable_hash.to_json
         render json: json_string
