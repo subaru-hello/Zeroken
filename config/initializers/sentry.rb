20f9f87f31f1ff60435160b6f83813116491167f
runteq-profile <octpsubaru@icloud.com>
2022-01-21 13:20:38 +0900

    error log #119

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,19 +1,12 @@
 Sentry.init do |config|
-    config.dsn = Rails.application.credentials.dig(:sentry, :dsn)
-    config.breadcrumbs_logger = [:active_support_logger]
-  
-    # ユーザIP、クッキーなどの個人を特定できる情報を送信したい場合はtrueにする
-    config.send_default_pii = false
-  
-    config.excluded_exceptions += %w[ActiveRecord::RecordNotFound]
-  
-    config.enabled_environments = %w[production]
-  
-    # To activate performance monitoring, set one of these options.
-    # We recommend adjusting the value in production:
-    config.traces_sample_rate = 0.5
-    # or
-    config.traces_sampler = lambda do |context|
-      true
-    end
+  config.dsn = 'https://examplePublicKey@o0.ingest.sentry.io/0'
+  config.breadcrumbs_logger = [:sentry_logger, :http_logger]
+
+  # To activate performance monitoring, set one of these options.
+  # We recommend adjusting the value in production:
+  config.traces_sample_rate = 0.5
+  # or
+  config.traces_sampler = lambda do |context|
+    0.5
   end
+end
