18d64bf96468c8aa938aff806901280d3cb64f1a
runteq-profile <octpsubaru@icloud.com>
2022-01-26 22:17:31 +0900

    lintチェック

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,5 +1,5 @@
 Sentry.init do |config|
-  config.dsn = 'https://examplePublicKey@o0.ingest.sentry.io/0'
+  config.dsn = 'https://2b1bf934f766404683660108f1121d2f@o1095713.ingest.sentry.io/6115539'
   config.breadcrumbs_logger = [:sentry_logger, :http_logger]
 
   # To activate performance monitoring, set one of these options.
