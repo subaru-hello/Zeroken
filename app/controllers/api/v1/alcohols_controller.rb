b1e3aafdc1f6111a466d5575d271379e91e17319
runteq-profile <octpsubaru@icloud.com>
2022-01-26 22:18:01 +0900

    ユーザーに紐づくお酒データ取得ロジック作成 #125

--- app/controllers/api/v1/alcohols_controller.rb
+++ app/controllers/api/v1/alcohols_controller.rb
@@ -6,11 +6,8 @@ module Api
       end
 
       def index
-        @alcohols = Alcohol.all
-        # どのお酒をanalyzeResultに保存するのかかく
-        total_alcohol_amounts = alcohol.alcohol_amount * alcohol.alcohol_percentage
-        # scopeに0、1200、1500、4000などを定義し、それぞれの条件に該当するお酒をフロントに全て渡す。
-        # フロント側で順番を変更し、AnalyzeResultに配列で渡し、AnalyzeResultは受け取った４つのお酒をalcohol_first,alcohol_second,alcohol_third,alcohol_forthに保存する　
+        alcohol_amount = current_user.analyze_results.last['total_alcohol_amounts']
+        @alcohols = Alcohol.new.sum_amount(alcohol_amount)
         respond_to { |format| format.json { render json: @alcohols, methods: [:image_url] } }
       end
 
@@ -28,13 +25,7 @@ module Api
       def alcohol_params
         params
           .require(:alcohol)
-          .permit(
-            :alcohol_percentage,
-            :alcohol_amount,
-            :name,
-            :description,
-            :image
-          )
+          .permit(:alcohol_percentage, :alcohol_amount, :name, :description, :image)
       end
     end
   end
