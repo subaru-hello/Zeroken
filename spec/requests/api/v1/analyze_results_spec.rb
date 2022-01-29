cb9638eba07998a9c0f9ab40f8c17ce0fb5217fa
runteq-profile <octpsubaru@icloud.com>
2022-01-30 07:34:23 +0900

    specを作成した

--- spec/requests/api/v1/analyze_results_spec.rb
+++ spec/requests/api/v1/analyze_results_spec.rb
@@ -1,15 +1,15 @@
 require 'rails_helper'
-RSpec.describe 'Analyzes', type: :request do
+RSpec.describe 'AnalyzeResults', type: :request do
 
   
   let(:headers) { { CONTENT_TYPE: 'application/json', ACCEPT: 'application/json' } }
 
   describe 'GET /analyzes' do
     let(:user) { create(:user) }
-    let(:analyze_num) { 5 }
-  let(:http_request) { get api_v1_analyzes_path, headers: headers }
+    let(:analyze_result_num) { 5 }
+  let(:http_request) { get api_v1_analyze_results_path, headers: headers }
   before do
-    create_list(:analyze, analyze_num, user: user)
+    create_list(:analyze_result, analyze_result_num, user: user)
   end
   context 'with access_token' do
     let!(:api_key) { create(:api_key, user: user) }
@@ -17,7 +17,7 @@ RSpec.describe 'Analyzes', type: :request do
 
     it 'returns analyzes in json format' do
       http_request
-      expect(json.count).to eq(analyze_num)
+      expect(json.count).to eq(analyze_result_num)
       expect(response).to be_successful
       expect(response).to have_http_status(200)
     end
@@ -25,20 +25,20 @@ RSpec.describe 'Analyzes', type: :request do
   it_should_behave_like 'unauthorized user'
 end
 
-  describe 'POST /analyzes' do
+  describe 'POST /analyze_results' do
     let(:user) { create(:user) }
     let!(:request_hash) { {} }
-    let(:http_request) { post api_v1_analyzes_path, request_hash }
+    let(:http_request) { post api_v1_analyze_results_path, request_hash }
     
     context 'with access_token' do
       let!(:api_key) { create(:api_key, user: user) }
       let(:headers) { { CONTENT_TYPE: 'application/json', ACCEPT: 'application/json', Authorization: "Bearer #{api_key.access_token}" } }
-      let!(:request_hash) { { headers: headers, params: { analyze: attributes_for(:analyze) }.to_json } }
+      let!(:request_hash) { { headers: headers, params: { analyze_result: attributes_for(:analyze_result) }.to_json } }
 
       it 'returns analyzes in json format' do
         expect {
           http_request
-        }.to change(Analyze, :count).by(1)
+        }.to change(AnalyzeResult, :count).by(1)
 
         expect(response).to be_successful
         expect(response).to have_http_status(201)
