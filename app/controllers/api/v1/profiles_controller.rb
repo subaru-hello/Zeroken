34cf788f98126872e76027fe3484bc2689ee0312
runteq-profile <octpsubaru@icloud.com>
2022-01-29 23:15:15 +0900

    updateを追加した

--- app/controllers/api/v1/profiles_controller.rb
+++ app/controllers/api/v1/profiles_controller.rb
@@ -1,9 +1,9 @@
 module Api
   module V1
     class ProfilesController < UsersController
-      before_action :require_login, :authenticate
+      before_action :require_login
       def update
-        if current_user.update(user_params)
+        if current_user.update(profile_params)
           json_string = UserSerializer.new(current_user).serializable_hash
           render json: json_string
         else
@@ -12,22 +12,23 @@ module Api
       end
 
       def edit
-        # @user = User.find_by!(id: current_user.id)
-        current_user.nickname = params[:nickname]
-        current_user.email = params[:email]
-        # render json: @user
+        @user = User.find(current_user.id)
       end
 
       def password
-        current_user.password_confirmation = params[:password_confirmation]
-        current_user.password = params[:password] ? head(:ok) : head(:bad_request)
+        if current_user.update(profile_params)
+          json_string = UserSerializer.new(current_user).serializable_hash
+          render json: json_string
+        else
+          head :bad_request
+        end
       end
 
       private
 
-      def user_params
+      def profile_params
         # params[:data][:user][:avatar] = uploadedfile_create(params[:data][:user][:avatar])
-        params.require(:user).permit(:nickname, :email, :password, :password_confirmation)
+        params.require(:profile).permit(:nickname, :email, :password, :password_confirmation)
       end
     end
   end
