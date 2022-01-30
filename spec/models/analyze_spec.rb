e1cc4d06943d8b1a32319d4a9cfd42db783e0b98
runteq-profile <octpsubaru@icloud.com>
2022-01-30 20:07:28 +0900

    analyzeからanalyze_resultに変更した

--- spec/models/analyze_spec.rb
+++ spec/models/analyze_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
-RSpec.describe Analyze, type: :model do
-  let(:analyze) { build(:analyze) }
+RSpec.describe AnalyzeResult, type: :model do
+  let(:analyze_result) { build(:analyze_result) }
   describe '診断項目が正しく作成されるかの検証' do
     context '診断内容が適切な場合' do
       valid_total_points = %w[ -10 10 1 0 3 4 ]
@@ -9,20 +9,20 @@ RSpec.describe Analyze, type: :model do
       valid_next_motivation = %w[ flesh tipsy heavy_drunk ]
       it 'total_pointsが作成される' do
         valid_total_points.each do |point|
-          analyze.total_points = point
-          expect(analyze).to be_valid
+          AnalyzeResult.total_points = point
+          expect(analyze_result).to be_valid
         end
       end
       it 'お酒の強さが作成される' do
         valid_alcohol_strongness.each do |strongness|
-          analyze.alcohol_strongness = strongness
-          expect(analyze).to be_valid
+          AnalyzeResult.alcohol_strongness = strongness
+          expect(analyze_result).to be_valid
         end
       end
       it '次の飲み会に向けた気分が作成される' do
         valid_next_motivation.each do |nomivation|
-          analyze.next_motivation = nomivation
-          expect(analyze).to be_valid
+          AnalyzeResult.next_motivation = nomivation
+          expect(analyze_result).to be_valid
         end
       end
     end
