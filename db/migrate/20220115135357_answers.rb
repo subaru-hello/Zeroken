bbdef4cd6847bb3cb6f0d5f88227c82fb5d8e624
runteq-profile <octpsubaru@icloud.com>
2022-01-31 07:38:03 +0900

    questionとanswerのアソシエーションを削除

--- db/migrate/20220115135357_answers.rb
+++ db/migrate/20220115135357_answers.rb
@@ -1,7 +1,6 @@
 class Answers < ActiveRecord::Migration[6.1]
   def change
     create_table :answers do |t|
-      t.references :questions, foreign_key: true
       t.integer :point
       t.string :choice
       t.timestamps
