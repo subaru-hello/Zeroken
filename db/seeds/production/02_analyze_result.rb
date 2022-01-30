4dbfcc132591599c43c7a30c0454f263762e650d
runteq-profile <octpsubaru@icloud.com>
2022-01-31 00:28:37 +0900

    seedファイル修正

--- db/seeds/production/02_analyze_result.rb
+++ db/seeds/production/02_analyze_result.rb
@@ -1,24 +1,24 @@
-AnalyzeReslut.seed do |s|
-    s.id = 1
-    s.user_id = 1
-    s.total_points = 1
-    s.alcohol_strongness ='strong'
-    s.next_motivation ='tipsy'
-    s.total_alcohol_amounts = 4000
-    s.alcohol_first = '水'
-    s.alcohol_second = 'ビール'
-    s.alcohol_third = '日本酒'
-    s.alcohol_forth = '和み水'
-    end
 AnalyzeResult.seed do |s|
     s.id = 1
     s.user_id = 1
-    s.total_points = 1
+    s.description = "こんにちは"
     s.alcohol_strongness ='strong'
     s.next_motivation ='tipsy'
     s.total_alcohol_amounts = 4000
-    s.alcohol_first = '水'
-    s.alcohol_second = 'ビール'
-    s.alcohol_third = '日本酒'
-    s.alcohol_forth = '和み水'
+    s.first_alcohol = 21
+    s.second_alcohol = 32
+    s.third_alcohol = 31
+    s.forth_alcohol = 33
+    end
+AnalyzeResult.seed do |s|
+    s.id = 1
+    s.user_id = 2
+    s.description = "こんにちは"
+    s.alcohol_strongness ='strong'
+    s.next_motivation ='tipsy'
+    s.total_alcohol_amounts = 4000
+    s.first_alcohol = 1
+    s.second_alcohol = 2
+    s.third_alcohol = 15
+    s.forth_alcohol = 12
     end
