ce44f587b063e7ff34b9bed653ee7e1b2f21db78
runteq-profile <octpsubaru@icloud.com>
2022-01-31 10:47:11 +0900

    seedファイル編集

--- db/seeds/production/06_answer.rb
+++ db/seeds/production/06_answer.rb
@@ -1,33 +1,33 @@
 Answer.seed(
-    
+    {
    point: -10.04,
    choice: "いつも"
 },
-
+{
    point: 8.95,
    choice: "時々"
 },
-
+{
    point: 5.22,
    choice: "全くない"
 },
-
+{
    point: -0.43,
    choice: "いつも"
 },
-
+{
    point: -2.98 ,
    choice: "時々"
 },
-
+{
    point: 1.2,
    choice: "全くない"
 },
-
+{
    point: 3.37,
    choice: "いつも"
 },
-
+{
    point: -3.89,
    choice: "時々"
 },
@@ -35,43 +35,43 @@ Answer.seed(
    point: 0.38,
    choice: "全くない"
 },
-
+{
    point: -0.58,
    choice: "いつも"
 },
-
+{
    point: -1.27,
    choice: "時々"
 },
-
+{
    point: 0.25,
    choice: "全くない"
 },
-
+{
    point: 0.31,
    choice: "いつも"
 },
-
+{
    point: 0.36,
    choice: "時々"
 },
-
+{
    point: -1.03,
    choice: "全くない"
 },
-
+{
    point: 0 ,
    choice: "いつも"
 },
-
+{
    point: -4.11,
    choice: "時々"
 },
-
+{
    point: 0.1,
    choice: "全くない"
 },
-
+{
    point: -0.79,
    choice: "いつも"
 },
@@ -79,31 +79,31 @@ Answer.seed(
    point: 0.07,
    choice: "時々"
 },
-
+{
    point: 0.01,
    choice: "全くない"
 },
-
+{
    point: 0.83 ,
    choice: "いつも"
 },
-
+{
    point: 0.62,
    choice: "時々"
 },
-
+{
    point: -0.24,
    choice: "全くない"
 },
-
+{
    point: -3.25,
    choice: "いつも"
 },
-
+{
    point: 1.43,
    choice: "時々"
 },
-
+{
    point: -0.44,
    choice: "全くない"
 },
