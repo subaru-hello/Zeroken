0b0af9d48a3de9aa0e71f8f6ea0e9ffcec137480
runteq-profile <octpsubaru@icloud.com>
2022-01-31 10:16:25 +0900

    dbファイルを編集した

--- db/migrate/20220131010237_remove_reference_from_answer.rb
+++
@@ -1,7 +0,0 @@
-class RemoveReferenceFromAnswer < ActiveRecord::Migration[6.1]
-  def change
-    remove_foreign_key :answers, :questions
-    remove_index :answers, :question_id
-    remove_reference :answers, :question
-  end
-end
--- db/seeds/production/06_answer.rb
+++ db/seeds/production/06_answer.rb
@@ -1,198 +1,159 @@
 Answer.seed(
-    {
-       questions_id: 1,
-       point: -10.04,
-       choice: "いつも"
-    },
-    {
-       questions_id: 1,
-       point: 8.95,
-       choice: "時々"
-    },
-    {
-       questions_id: 1,
-       point: 5.22,
-       choice: "全くない"
-    },
-    {
-       questions_id: 2,
-       point: -0.43,
-       choice: "いつも"
-    },
-    {
-       questions_id: 2,
-       point: -2.98 ,
-       choice: "時々"
-    },
-    {
-       questions_id: 2,
-       point: 1.2,
-       choice: "全くない"
-    },
-    {
-       questions_id: 3,
-       point: 3.37,
-       choice: "いつも"
-    },
-    {
-       questions_id: 3,
-       point: -3.89,
-       choice: "時々"
-    },
-    {
-       questions_id: 3,
-       point: 0.38,
-       choice: "全くない"
-    },
-    {
-       questions_id: 4,
-       point: -0.58,
-       choice: "いつも"
-    },
-    {
-       questions_id: 4,
-       point: -1.27,
-       choice: "時々"
-    },
-    {
-       questions_id: 4,
-       point: 0.25,
-       choice: "全くない"
-    },
-    {
-       questions_id: 5,
-       point: 0.31,
-       choice: "いつも"
-    },
-    {
-       questions_id: 5,
-       point: 0.36,
-       choice: "時々"
-    },
-    {
-       questions_id: 5,
-       point: -1.03,
-       choice: "全くない"
-    },
-    {
-       questions_id: 6,
-       point: 0 ,
-       choice: "いつも"
-    },
-    {
-       questions_id: 6,
-       point: -4.11,
-       choice: "時々"
-    },
-    {
-       questions_id: 6,
-       point: 0.1,
-       choice: "全くない"
-    },
-    {
-       questions_id: 7,
-       point: -0.79,
-       choice: "いつも"
-    },
-    {
-       questions_id: 7,
-       point: 0.07,
-       choice: "時々"
-    },
-    {
-       questions_id: 7,
-       point: 0.01,
-       choice: "全くない"
-    },
-    {
-       questions_id: 8,
-       point: 0.83 ,
-       choice: "いつも"
-    },
-    {
-       questions_id: 8,
-       point: 0.62,
-       choice: "時々"
-    },
-    {
-       questions_id: 8,
-       point: -0.24,
-       choice: "全くない"
-    },
-    {
-       questions_id: 9,
-       point: -3.25,
-       choice: "いつも"
-    },
-    {
-       questions_id: 9,
-       point: 1.43,
-       choice: "時々"
-    },
-    {
-       questions_id: 9,
-       point: -0.44,
-       choice: "全くない"
-    },
-    {
-       questions_id: 10,
-       point: -3.25 ,
-       choice: "いつも"
-    },
-    {
-       questions_id: 10,
-       point: 1.43,
-       choice: "時々"
-    },
-    {
-       questions_id: 10,
-       point: -0.44,
-       choice: "全くない"
-    },
-    {
-       questions_id: 11,
-       point: -10.07,
-       choice: "いつも"
-    },
-    {
-       questions_id: 11,
-       point: -0.79 ,
-       choice: "時々"
-    },
-    {
-       questions_id: 11,
-       point: 10.8,
-       choice: "全くない"
-    },
-    {
-       questions_id: 12,
-       point: 8.15,
-       choice: "いつも"
-    },
-    {
-       questions_id: 12,
-       point: -2.42,
-       choice: "時々"
-    },
-    {
-       questions_id: 12,
-       point: 0.14,
-       choice: "全くない"
-    },
-    {
-       questions_id: 13,
-       point: -4.34 ,
-       choice: "いつも"
-    },
-    {
-       questions_id: 13,
-       point: 2.69 ,
-       choice: "時々"
-    },
-    {
-       questions_id: 13,
-       point: -0.19,
-       choice: "全くない"
-    },
-   
+    
+   point: -10.04,
+   choice: "いつも"
+},
+
+   point: 8.95,
+   choice: "時々"
+},
+
+   point: 5.22,
+   choice: "全くない"
+},
+
+   point: -0.43,
+   choice: "いつも"
+},
+
+   point: -2.98 ,
+   choice: "時々"
+},
+
+   point: 1.2,
+   choice: "全くない"
+},
+
+   point: 3.37,
+   choice: "いつも"
+},
+
+   point: -3.89,
+   choice: "時々"
+},
+
+   point: 0.38,
+   choice: "全くない"
+},
+
+   point: -0.58,
+   choice: "いつも"
+},
+
+   point: -1.27,
+   choice: "時々"
+},
+
+   point: 0.25,
+   choice: "全くない"
+},
+
+   point: 0.31,
+   choice: "いつも"
+},
+
+   point: 0.36,
+   choice: "時々"
+},
+
+   point: -1.03,
+   choice: "全くない"
+},
+
+   point: 0 ,
+   choice: "いつも"
+},
+
+   point: -4.11,
+   choice: "時々"
+},
+
+   point: 0.1,
+   choice: "全くない"
+},
+
+   point: -0.79,
+   choice: "いつも"
+},
+{
+   point: 0.07,
+   choice: "時々"
+},
+
+   point: 0.01,
+   choice: "全くない"
+},
+
+   point: 0.83 ,
+   choice: "いつも"
+},
+
+   point: 0.62,
+   choice: "時々"
+},
+
+   point: -0.24,
+   choice: "全くない"
+},
+
+   point: -3.25,
+   choice: "いつも"
+},
+
+   point: 1.43,
+   choice: "時々"
+},
+
+   point: -0.44,
+   choice: "全くない"
+},
+{
+   point: -3.25 ,
+   choice: "いつも"
+},
+{
+   point: 1.43,
+   choice: "時々"
+},
+{
+   point: -0.44,
+   choice: "全くない"
+},
+{
+   point: -10.07,
+   choice: "いつも"
+},
+{
+   point: -0.79 ,
+   choice: "時々"
+},
+{
+   point: 10.8,
+   choice: "全くない"
+},
+{
+   point: 8.15,
+   choice: "いつも"
+},
+{
+   point: -2.42,
+   choice: "時々"
+},
+{
+   point: 0.14,
+   choice: "全くない"
+},
+{
+   point: -4.34 ,
+   choice: "いつも"
+},
+{
+   point: 2.69 ,
+   choice: "時々"
+},
+{
+   point: -0.19,
+   choice: "全くない"
+},
+
 )
