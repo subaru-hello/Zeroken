9a560b73bf128b3c8746e9e3597495b8d9a55091
runteq-profile <octpsubaru@icloud.com>
2022-01-31 17:08:41 +0900

    Lintチェック実行

--- app/models/analyze_result.rb
+++ app/models/analyze_result.rb
@@ -5,90 +5,90 @@ class AnalyzeResult < ApplicationRecord
   enum next_motivation: { flesh: 0, tipsy: 1, heavy_drunk: 2 }
   enum alcohol_strongness: { weak: 0, weak_normal: 1, normal: 2, normal_strong: 3, strong: 4 }
 
-    def self.cal_shuchedule(weight, next_motivation, user_id)
-      total_point = AnalyzeResult.cal_total_point(user_id)
-      description = ExtractDescription.which_extract_description(total_point)
-      alcohol_strongness = AnalyzeResult.cal_alcohol_strongness(total_point)
-      total_alcohol_amounts =
-        AnalyzeResult.cal_total_alcohol_amount(weight, alcohol_strongness, next_motivation)
-      alcohols = Alcohol.sum_amount(total_alcohol_amounts)
-      shuchedule = alcohols.map(&:sample)
+  def self.cal_shuchedule(weight, next_motivation, user_id)
+    total_point = AnalyzeResult.cal_total_point(user_id)
+    description = ExtractDescription.which_extract_description(total_point)
+    alcohol_strongness = AnalyzeResult.cal_alcohol_strongness(total_point)
+    total_alcohol_amounts =
+      AnalyzeResult.cal_total_alcohol_amount(weight, alcohol_strongness, next_motivation)
+    alcohols = Alcohol.sum_amount(total_alcohol_amounts)
+    shuchedule = alcohols.map(&:sample)
 
-      {
-        user_id: user_id,
-        next_motivation: next_motivation,
-        description: description,
-        alcohol_strongness: alcohol_strongness,
-        total_alcohol_amounts: total_alcohol_amounts,
-        first_alcohol: shuchedule[0]['id'],
-        second_alcohol: shuchedule[1]['id'],
-        third_alcohol: shuchedule[2]['id'],
-        forth_alcohol: shuchedule[3]['id']
-      }
-    end
-    
-    def self.cal_total_point(user_id)
-      tast_answer_array = TastAnswer.where(user_id: user_id).pluck[0][2..14]
+    {
+      user_id: user_id,
+      next_motivation: next_motivation,
+      description: description,
+      alcohol_strongness: alcohol_strongness,
+      total_alcohol_amounts: total_alcohol_amounts,
+      first_alcohol: shuchedule[0]['id'],
+      second_alcohol: shuchedule[1]['id'],
+      third_alcohol: shuchedule[2]['id'],
+      forth_alcohol: shuchedule[3]['id']
+    }
+  end
 
-      target_array = tast_answer_array.map.with_index { |arr, i| 10 * (arr + 3 * i) -5 }
-      result = target_array.map { |n| Answer.find(n).point.to_f }
+  def self.cal_total_point(user_id)
+    tast_answer_array = TastAnswer.where(user_id: user_id).pluck[0][2..14]
 
-      result.sum
-    end
+    target_array = tast_answer_array.map.with_index { |arr, i| (10 * (arr + (3 * i))) - 5 }
+    result = target_array.map { |n| Answer.find(n).point.to_f }
 
-    def self.cal_total_alcohol_amount(weight, alcohol_strongness, next_motivation)
-      pair = [alcohol_strongness, next_motivation]
-      check_alcohol_in_vein =
-        case pair
-        when [4, 0]
-          5
-        when [4, 1]
-          15
-        when [4, 2]
-          25
-        when [3, 0]
-          35
-        when [3, 1]
-          45
-        when [3, 2]
-          55
-        when [2, 0]
-          65
-        when [2, 1]
-          75
-        when [2, 2]
-          85
-        when [1, 0]
-          95
-        when [1, 1]
-          105
-        when [1, 2]
-          115
-        when [0, 0]
-          125
-        when [0, 1]
-          135
-        when [0, 2]
-          145
-        end
-      alcohol_in_vein = AlcoholInVein.find(check_alcohol_in_vein).percentage
+    result.sum
+  end
 
-      coefficient = 833
-      weight * coefficient * alcohol_in_vein / 100
-    end
-
-    def self.cal_alcohol_strongness(total_point)
-      case total_point
-      when 3..30
-        4
-      when 0...3
-        3
-      when -3...0
-        2
-      when -30...-3
-        1
-      else
-        0
+  def self.cal_total_alcohol_amount(weight, alcohol_strongness, next_motivation)
+    pair = [alcohol_strongness, next_motivation]
+    check_alcohol_in_vein =
+      case pair
+      when [4, 0]
+        5
+      when [4, 1]
+        15
+      when [4, 2]
+        25
+      when [3, 0]
+        35
+      when [3, 1]
+        45
+      when [3, 2]
+        55
+      when [2, 0]
+        65
+      when [2, 1]
+        75
+      when [2, 2]
+        85
+      when [1, 0]
+        95
+      when [1, 1]
+        105
+      when [1, 2]
+        115
+      when [0, 0]
+        125
+      when [0, 1]
+        135
+      when [0, 2]
+        145
       end
+    alcohol_in_vein = AlcoholInVein.find(check_alcohol_in_vein).percentage
+
+    coefficient = 833
+    weight * coefficient * alcohol_in_vein / 100
+  end
+
+  def self.cal_alcohol_strongness(total_point)
+    case total_point
+    when 3..30
+      4
+    when 0...3
+      3
+    when -3...0
+      2
+    when -30...-3
+      1
+    else
+      0
     end
+  end
 end
--- app/models/concerns/extract_description.rb
+++ app/models/concerns/extract_description.rb
@@ -1,50 +1,48 @@
 module ExtractDescription
   extend ActiveSupport::Concern
-    EXTRACT_DESCRIPTIONS = [
-      { total_point_range: -30...-20, cal: 5 }, 
-      { total_point_range: -20...-19, cal: 15 },
-      { total_point_range: -18...-17, cal: 25 },
-      { total_point_range: -17...-16, cal: 35 },
-      { total_point_range: -16...-15, cal: 45 },
-      { total_point_range: -15...-14, cal: 55 },
-      { total_point_range: -14...-13, cal: 65 },
-      { total_point_range: -13...-12, cal: 75 },
-      { total_point_range: -12...-11, cal: 85 },
-      { total_point_range: -11...-10, cal: 95 },
-      { total_point_range: -10...-9, cal: 105 },
-      { total_point_range: -9...-8, cal: 115 },
-      { total_point_range: -8...-7, cal: 125 },
-      { total_point_range: -7...-6, cal: 135 },
-      { total_point_range: -6...-5, cal: 145 },
-      { total_point_range: -5...-4, cal: 155 },
-      { total_point_range: -4...-3, cal: 165 },
-      { total_point_range: -3...-2, cal: 175 },
-      { total_point_range: -1...0, cal: 185 },
-      { total_point_range: 0...1, cal: 195 },
-      { total_point_range: 1...2, cal: 205 },
-      { total_point_range: 2...3, cal: 215 },
-      { total_point_range: 3...4, cal: 225 },
-      { total_point_range: 4...5, cal: 235 },
-      { total_point_range: 5...6, cal: 245 },
-      { total_point_range: 6...7, cal: 255 },
-      { total_point_range: 7...8, cal: 265 },
-      { total_point_range: 8...9, cal: 275 },
-      { total_point_range: 9...10, cal: 285 },
-      { total_point_range: 10...11, cal: 295 },
-      { total_point_range: 11...12, cal: 305 },
-      { total_point_range: 12...13, cal: 315 },
-      { total_point_range: 13...14, cal: 325 },
-      { total_point_range: 14...15, cal: 335 },
-      { total_point_range: 15...16, cal: 345 },
-      { total_point_range: 16...17, cal: 355 },
-      { total_point_range: 17...30, cal: 365 },
-    ]
-    
-    def self.which_extract_description(total_point)
-      EXTRACT_DESCRIPTIONS.each do |extract_description|
-        if extract_description[:total_point_range] === total_point
-          return extract_description[:cal]
-        end
-      end
+  EXTRACT_DESCRIPTIONS = [
+    { total_point_range: -30...-20, cal: 5 },
+    { total_point_range: -20...-19, cal: 15 },
+    { total_point_range: -18...-17, cal: 25 },
+    { total_point_range: -17...-16, cal: 35 },
+    { total_point_range: -16...-15, cal: 45 },
+    { total_point_range: -15...-14, cal: 55 },
+    { total_point_range: -14...-13, cal: 65 },
+    { total_point_range: -13...-12, cal: 75 },
+    { total_point_range: -12...-11, cal: 85 },
+    { total_point_range: -11...-10, cal: 95 },
+    { total_point_range: -10...-9, cal: 105 },
+    { total_point_range: -9...-8, cal: 115 },
+    { total_point_range: -8...-7, cal: 125 },
+    { total_point_range: -7...-6, cal: 135 },
+    { total_point_range: -6...-5, cal: 145 },
+    { total_point_range: -5...-4, cal: 155 },
+    { total_point_range: -4...-3, cal: 165 },
+    { total_point_range: -3...-2, cal: 175 },
+    { total_point_range: -1...0, cal: 185 },
+    { total_point_range: 0...1, cal: 195 },
+    { total_point_range: 1...2, cal: 205 },
+    { total_point_range: 2...3, cal: 215 },
+    { total_point_range: 3...4, cal: 225 },
+    { total_point_range: 4...5, cal: 235 },
+    { total_point_range: 5...6, cal: 245 },
+    { total_point_range: 6...7, cal: 255 },
+    { total_point_range: 7...8, cal: 265 },
+    { total_point_range: 8...9, cal: 275 },
+    { total_point_range: 9...10, cal: 285 },
+    { total_point_range: 10...11, cal: 295 },
+    { total_point_range: 11...12, cal: 305 },
+    { total_point_range: 12...13, cal: 315 },
+    { total_point_range: 13...14, cal: 325 },
+    { total_point_range: 14...15, cal: 335 },
+    { total_point_range: 15...16, cal: 345 },
+    { total_point_range: 16...17, cal: 355 },
+    { total_point_range: 17...30, cal: 365 }
+  ].freeze
+
+  def self.which_extract_description(total_point)
+    EXTRACT_DESCRIPTIONS.each do |extract_description|
+      return extract_description[:cal] if extract_description[:total_point_range] == total_point
     end
+  end
 end
